d98c08a80040bfe89ee3ceabbae16ae6245e0b4c
Steve Springett <steve@springett.us>
2013-08-20 16:42:08 -0700

    Added Dependency-Check integration by incorporating the data update procedure and making available a zip archive of the data directory. Minor cleanup and checkstyle changes.

---
+++ src/main/java/org/owasp/dependencytrack/tasks/DependencyCheckDataUpdater.java
@@ -0,0 +1,92 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Dependency-Track is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Dependency-Track is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Dependency-Track. If not, see http://www.gnu.org/licenses/.
+ *
+ * Copyright (c) Axway. All Rights Reserved.
+ */
+
+package org.owasp.dependencytrack.tasks;
+
+import org.owasp.dependencycheck.data.CachedWebDataSource;
+import org.owasp.dependencycheck.data.UpdateException;
+import org.owasp.dependencycheck.data.UpdateService;
+import org.owasp.dependencycheck.utils.Settings;
+import org.owasp.dependencytrack.Constants;
+import org.owasp.dependencytrack.util.ZipUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class DependencyCheckDataUpdater {
+
+    /**
+     * Setup logger
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(DependencyCheckDataUpdater.class);
+
+
+    /**
+     * Updates the Dependency-Check data directory.
+     */
+    @Scheduled(fixedRate = 86400000) // every 24 hours
+    public void doUpdates() {
+        // Configure the data directory
+        Settings.setString(Settings.KEYS.DATA_DIRECTORY, Constants.DATA_DIR);
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("Performing scheduled update of Dependency-Check data directory");
+        }
+
+        //Cycles through the cached web data sources and calls update on all of them.
+        final UpdateService service = UpdateService.getInstance();
+        final Iterator<CachedWebDataSource> iterator = service.getDataSources();
+        while (iterator.hasNext()) {
+            final CachedWebDataSource source = iterator.next();
+            try {
+                source.update();
+                packageDataDirectory();
+            } catch (UpdateException ex) {
+                if (LOGGER.isWarnEnabled()) {
+                    LOGGER.warn("Unable to update Cached Web DataSource");
+                }
+            }
+        }
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("Update complete");
+        }
+    }
+
+    /**
+     * Packages the Dependency-Check data directory into a ZIP archive.
+     */
+    private void packageDataDirectory() {
+        try {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Packaging Dependency-Check data directory");
+            }
+            ZipUtil.createZip(Constants.DATA_DIR, Constants.DATA_ZIP);
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Dependency-Check data directory packaging complete");
+            }
+        } catch (IOException e) {
+            if (LOGGER.isWarnEnabled()) {
+                LOGGER.warn("An error occurred packaging the data directory: " + e.getMessage());
+            }
+        }
+    }
+
+}
---
+++ src/main/java/org/owasp/dependencytrack/util/ZipUtil.java
@@ -0,0 +1,104 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Dependency-Track is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Dependency-Track is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Dependency-Track. If not, see http://www.gnu.org/licenses/.
+ *
+ * Copyright (c) Axway. All Rights Reserved.
+ */
+
+package org.owasp.dependencytrack.util;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+import org.apache.commons.io.IOUtils;
+
+/**
+ * Zip utilities.
+ */
+public final class ZipUtil {
+
+    /**
+     * Private constructor.
+     */
+    private ZipUtil() { }
+
+    /**
+     * Creates a zip file at the specified path with the contents of the specified directory.
+     *
+     * @param directoryPath The path of the directory where the archive will be created. eg. c:/temp
+     * @param zipPath The full path of the archive to create. eg. c:/temp/archive.zip
+     * @throws IOException If anything goes wrong
+     */
+    public static void createZip(String directoryPath, String zipPath) throws IOException {
+        FileOutputStream fOut = null;
+        BufferedOutputStream bOut = null;
+        ZipArchiveOutputStream tOut = null;
+
+        try {
+            fOut = new FileOutputStream(new File(zipPath));
+            bOut = new BufferedOutputStream(fOut);
+            tOut = new ZipArchiveOutputStream(bOut);
+            addFileToZip(tOut, directoryPath, "");
+            tOut.finish();
+        } finally {
+            IOUtils.closeQuietly(tOut);
+            IOUtils.closeQuietly(bOut);
+            IOUtils.closeQuietly(fOut);
+        }
+    }
+
+    /**
+     * Creates a zip entry for the path specified with a name built from the base passed in and the file/directory
+     * name. If the path is a directory, a recursive call is made such that the full directory is added to the zip.
+     *
+     * @param zOut The zip file's output stream
+     * @param path The filesystem path of the file/directory being added
+     * @param base The base prefix to for the name of the zip file entry
+     *
+     * @throws IOException If anything goes wrong
+     */
+    private static void addFileToZip(ZipArchiveOutputStream zOut, String path, String base) throws IOException {
+        final File f = new File(path);
+        final String entryName = base + f.getName();
+        final ZipArchiveEntry zipEntry = new ZipArchiveEntry(f, entryName);
+
+        zOut.putArchiveEntry(zipEntry);
+
+        if (f.isFile()) {
+            FileInputStream fInputStream = null;
+            try {
+                fInputStream = new FileInputStream(f);
+                IOUtils.copy(fInputStream, zOut);
+                zOut.closeArchiveEntry();
+            } finally {
+                IOUtils.closeQuietly(fInputStream);
+            }
+
+        } else {
+            zOut.closeArchiveEntry();
+            final File[] children = f.listFiles();
+
+            if (children != null) {
+                for (File child : children) {
+                    addFileToZip(zOut, child.getAbsolutePath(), entryName + "/");
+                }
+            }
+        }
+    }
+}

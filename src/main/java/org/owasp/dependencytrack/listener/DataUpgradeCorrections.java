ab76941c74536cf06578802fe390de63ac859b2a
Steve Springett <steve@springett.us>
2015-06-11 17:55:20 -0500

    Removing unused import

--- src/main/java/org/owasp/dependencytrack/listener/DataUpgradeCorrections.java
+++ src/main/java/org/owasp/dependencytrack/listener/DataUpgradeCorrections.java
@@ -28,7 +28,6 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.List;
 
 

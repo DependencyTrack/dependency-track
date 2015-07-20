98fd253cb2ab326d7aaa2b8d4d9e052c1d82b864
Tomas Bezdek <tbezdek@gumtree.com>
2015-07-20 10:44:31 +0100

    dependency-check-maven plugin needs Last-Modified header

--- src/main/java/org/owasp/dependencytrack/controller/DownloadController.java
+++ src/main/java/org/owasp/dependencytrack/controller/DownloadController.java
@@ -93,12 +93,14 @@ public class DownloadController extends AbstractController {
         InputStream fis = null;
         OutputStream out = null;
         try {
-            fis = new FileInputStream(Constants.NIST_DIR + File.separator + filename);
+            File file = new File(Constants.NIST_DIR + File.separator + filename);
+            fis = new FileInputStream(file);
             if (filename.endsWith(".gz")) {
                 response.setHeader("Content-Type", "application/x-gzip;");
             } else if (filename.endsWith(".xml")) {
                 response.setHeader("Content-Type", "application/xml;");
             }
+            response.addDateHeader("Last-Modified", file.lastModified());
             out = response.getOutputStream();
             IOUtils.copy(fis, out);
             out.flush();

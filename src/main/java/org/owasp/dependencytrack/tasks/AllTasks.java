58179f719bc7a779a4fcf903f0d1ce47ae5f204b
jason <jason@joeymail.net>
2015-12-16 08:10:30 +1100

    Added AllTasks to component scan task beans

---
+++ src/main/java/org/owasp/dependencytrack/tasks/AllTasks.java
@@ -0,0 +1,11 @@
+package org.owasp.dependencytrack.tasks;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Jason Wraxall on 25/11/15.
+ */
+@Configuration
+public class AllTasks {
+
+}

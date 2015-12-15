f986bf6797bbfee42691aca46b2d7f835bba971c
Jason Wraxall <jason@cybernostics.com.au>
2015-12-15 17:29:34 +1100

    Updated config for ApplicationDaoTest

--- src/test/java/org/owasp/dependencytrack/dao/ApplicationDaoTest.java
+++ src/test/java/org/owasp/dependencytrack/dao/ApplicationDaoTest.java
@@ -1,12 +1,15 @@
 package org.owasp.dependencytrack.dao;
 
+import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.owasp.dependencytrack.config.ApplicationConfiguration;
-import org.owasp.dependencytrack.config.DatabaseConfguration;
+import org.owasp.dependencytrack.config.DatabaseConfiguration;
+import org.owasp.dependencytrack.model.AllEntities;
 import org.owasp.dependencytrack.model.Application;
-import org.owasp.dependencytrack.service.AllServices;
+import org.owasp.dependencytrack.repository.AllRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,17 +25,20 @@ import static org.junit.Assert.assertThat;
  * Created by jason on 15/11/15.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {ApplicationConfiguration.class,
-        AllDaos.class, AllServices.class, DatabaseConfguration.class
-})
+@ContextConfiguration(classes = {DatabaseConfiguration.class,AllEntities.class,HibernateJpaAutoConfiguration.class, AllRepositories.class,AllDaos.class})
+@Rollback
 public class ApplicationDaoTest {
 
     @Autowired
     ApplicationDao applicationDao;
 
+    @Autowired
+    SessionFactory sessionFactory;
+
     @Test
     @Transactional
     public void testApplicationDaoMethods() throws Exception {
+        assertThat(sessionFactory, not(nullValue()));
         assertThat(applicationDao, not(nullValue()));
         List<Application> applications = applicationDao.listApplications();
         assertThat(applications, not(nullValue()));

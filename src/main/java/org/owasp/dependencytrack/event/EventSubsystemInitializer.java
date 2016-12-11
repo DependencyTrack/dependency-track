673c7d259042ffaa4f67678f009cc250019c83eb
Steve Springett <steve@springett.us>
2016-12-10 19:41:18 -0600

    Adding startup of event subsystem

---
+++ src/main/java/org/owasp/dependencytrack/event/EventSubsystemInitializer.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Dependency-Track is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Dependency-Track is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Dependency-Track. If not, see http://www.gnu.org/licenses/.
+ */
+package org.owasp.dependencytrack.event;
+
+import org.owasp.dependencytrack.event.framework.EventService;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class EventSubsystemInitializer implements ServletContextListener {
+
+    // Starts the EventService
+    private static final EventService eventService = EventService.getInstance();
+
+    public void contextInitialized(ServletContextEvent event) {
+    }
+
+    public void contextDestroyed(ServletContextEvent event) {
+        eventService.shutdown();
+    }
+}

71539d728513fa6792e35cae3d163c232ef947d7
Steve Springett <steve@springett.us>
2017-01-29 21:19:19 -0600

    Added persistence support for key/value store on a per project basis. #40

---
+++ src/main/java/org/owasp/dependencytrack/model/ProjectProperty.java
@@ -0,0 +1,84 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Dependency-Track is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Dependency-Track is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Dependency-Track. If not, see http://www.gnu.org/licenses/.
+ */
+package org.owasp.dependencytrack.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import javax.jdo.annotations.Column;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+import java.io.Serializable;
+
+@PersistenceCapable
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ProjectProperty implements Serializable {
+
+    private static final long serialVersionUID = -821103184547741489L;
+
+    @PrimaryKey
+    @Persistent(valueStrategy= IdGeneratorStrategy.INCREMENT)
+    @JsonIgnore
+    private long id;
+
+    @Persistent
+    @Column(name="PROJECT_ID", allowsNull="false")
+    @JsonIgnore
+    private long projectId;
+
+    @Persistent
+    @Column(name="KEY", jdbcType="VARCHAR", length=255, allowsNull="false")
+    private String key;
+
+    @Persistent
+    @Column(name="VALUE", jdbcType="VARCHAR", length=255, allowsNull="false")
+    private String value;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}

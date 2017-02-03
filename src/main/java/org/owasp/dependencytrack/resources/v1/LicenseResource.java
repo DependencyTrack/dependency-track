91585438977b45b24756b0226e078af62a4edd0b
Steve Springett <steve@springett.us>
2017-02-02 22:48:21 -0600

    Added license resource

---
+++ src/main/java/org/owasp/dependencytrack/resources/v1/LicenseResource.java
@@ -0,0 +1,79 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Dependency-Track is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Dependency-Track is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Dependency-Track. If not, see http://www.gnu.org/licenses/.
+ */
+package org.owasp.dependencytrack.resources.v1;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.owasp.dependencytrack.model.License;
+import org.owasp.dependencytrack.persistence.QueryManager;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/v1/license")
+@Api(value = "license")
+public class LicenseResource extends BaseResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Returns a list of all licenses",
+            response = License.class,
+            responseContainer = "List"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success")
+    })
+    public Response getTeams() {
+        try (QueryManager qm = new QueryManager()) {
+            List<License> licenses = qm.getLicenses();
+            return Response.ok(licenses).build();
+        }
+    }
+
+    @GET
+    @Path("/{licenseId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Returns a specific license",
+            response = License.class
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 404, message = "The license could not be found")
+    })
+    public Response getLicense(
+            @ApiParam(value = "The SPDX License ID of the license to retrieve", required = true)
+            @PathParam("licenseId") String licenseId) {
+        try (QueryManager qm = new QueryManager()) {
+            License license = qm.getLicense(licenseId);
+            if (license != null) {
+                return Response.ok(license).build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).entity("The license could not be found.").build();
+            }
+        }
+    }
+
+}

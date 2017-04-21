915c895804bd86542bf2483511b66717168a1a9d
Steve Springett <steve@springett.us>
2017-04-20 23:43:16 -0500

    Added component version resolver

---
+++ src/main/java/org/owasp/dependencytrack/parser/dependencycheck/resolver/ComponentVersionResolver.java
@@ -0,0 +1,52 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Dependency-Track is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Dependency-Track is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Dependency-Track. If not, see http://www.gnu.org/licenses/.
+ */
+package org.owasp.dependencytrack.parser.dependencycheck.resolver;
+
+import org.owasp.dependencytrack.parser.dependencycheck.model.Dependency;
+import org.owasp.dependencytrack.parser.dependencycheck.model.Evidence;
+import java.util.List;
+
+/**
+ * Attempts to resolve the version of the component from evidence
+ * available in the specified dependency.
+ */
+public class ComponentVersionResolver implements IResolver {
+
+    /**
+     * {@inheritDoc}
+     */
+    public String resolve(Dependency dependency) {
+        Evidence best = null;
+        final List<Evidence> evidenceList = dependency.getEvidenceCollected();
+        for (Evidence evidence: evidenceList) {
+            if ("version".equals(evidence.getType())) {
+                if ("file".equals(evidence.getSource()) && "HIGHEST".equals(evidence.getConfidence())) {
+                    return evidence.getValue();
+                }
+                if (best == null || (evidence.getConfidenceScore() > best.getConfidenceScore())) {
+                    if (evidence.getConfidenceScore() >= 2) { // We only want MEDIUM, HIGH or HIGHEST
+                        best = evidence;
+                    }
+                }
+            }
+        }
+        if (best != null) {
+            return best.getValue();
+        }
+        return null;
+    }
+}
--- src/main/java/org/owasp/dependencytrack/tasks/ScanModeler.java
+++ src/main/java/org/owasp/dependencytrack/tasks/ScanModeler.java
@@ -30,6 +30,7 @@ import org.owasp.dependencytrack.parser.dependencycheck.model.Analysis;
 import org.owasp.dependencytrack.parser.dependencycheck.model.Dependency;
 import org.owasp.dependencytrack.parser.dependencycheck.model.Evidence;
 import org.owasp.dependencytrack.parser.dependencycheck.resolver.ComponentResolver;
+import org.owasp.dependencytrack.parser.dependencycheck.resolver.ComponentVersionResolver;
 import org.owasp.dependencytrack.parser.dependencycheck.resolver.LicenseResolver;
 import org.owasp.dependencytrack.persistence.QueryManager;
 import java.io.File;
@@ -72,7 +73,7 @@ public class ScanModeler implements Subscriber {
                         // Component could not be resolved (was null), so create a new component
                         component = qm.createComponent(
                                 dependency.getFileName(), // name
-                                null, // version
+                                new ComponentVersionResolver().resolve(dependency),
                                 null, // group
                                 dependency.getFileName(),
                                 dependency.getMd5(),

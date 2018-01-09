08b709617e911f369f58b6485c5277bd52e257e9
Steve Springett <steve@springett.us>
2018-01-08 22:50:11 -0600

    Added BomResource desinged to handle the importing of CycloneDX and SPDX documents #52

---
+++ src/main/java/org/owasp/dependencytrack/resources/v1/BomResource.java
@@ -0,0 +1,82 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) Steve Springett. All Rights Reserved.
+ */
+package org.owasp.dependencytrack.resources.v1;
+
+import alpine.auth.PermissionRequired;
+import alpine.event.framework.EventService;
+import alpine.resources.AlpineResource;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
+import org.owasp.dependencytrack.auth.Permission;
+import org.owasp.dependencytrack.event.BomUploadEvent;
+import org.owasp.dependencytrack.model.Project;
+import org.owasp.dependencytrack.persistence.QueryManager;
+import org.owasp.dependencytrack.resources.v1.vo.BomSubmitRequest;
+import javax.validation.Validator;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Base64;
+
+/**
+ * JAX-RS resources for processing bill-of-material (bom) documents.
+ *
+ * @author Steve Springett
+ * @since 3.0.0
+ */
+@Path("/v1/bom")
+@Api(value = "bom", authorizations = @Authorization(value = "X-Api-Key"))
+public class BomResource extends AlpineResource {
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Upload a supported bil of material format document",
+            notes = "Expects CycloneDX or SPDX (text or RDF) along and a valid project UUID"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 404, message = "The project could not be found")
+    })
+    @PermissionRequired(Permission.SCAN_UPLOAD)
+    public Response uploadScan(BomSubmitRequest request) {
+        final Validator validator = getValidator();
+        failOnValidationError(
+                validator.validateProperty(request, "project"),
+                validator.validateProperty(request, "bom")
+        );
+        try (QueryManager qm = new QueryManager()) {
+            final Project project = qm.getObjectByUuid(Project.class, request.getProject());
+            if (project != null) {
+                final byte[] decoded = Base64.getDecoder().decode(request.getBom());
+                EventService.getInstance().publish(new BomUploadEvent(project.getUuid(), decoded));
+                return Response.ok().build();
+            } else {
+                return Response.status(Response.Status.NOT_FOUND).entity("The project could not be found.").build();
+            }
+        }
+    }
+
+}
---
+++ src/main/java/org/owasp/dependencytrack/resources/v1/vo/BomSubmitRequest.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) Steve Springett. All Rights Reserved.
+ */
+package org.owasp.dependencytrack.resources.v1.vo;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+/**
+ * Defines a custom request object used when uploading bill-of-material (bom) documents.
+ *
+ * @author Steve Springett
+ * @since 3.0.0
+ */
+public final class BomSubmitRequest {
+
+    @NotNull
+    @Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", message = "The project must be a valid 36 character UUID")
+    private final String project;
+
+    @NotNull
+    @Pattern(regexp = "^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$", message = "The BOM must be Base64 encoded")
+    private final String bom;
+
+    @JsonCreator
+    public BomSubmitRequest(@JsonProperty(value = "project", required = true) String project,
+                             @JsonProperty(value = "bom", required = true) String bom) {
+        this.project = project;
+        this.bom = bom;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public String getBom() {
+        return bom;
+    }
+
+}

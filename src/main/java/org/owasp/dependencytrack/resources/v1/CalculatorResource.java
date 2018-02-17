bbae89a5d37db3015d0d2d9e7c7dd78cfbc3924a
Steve Springett <steve@springett.us>
2018-02-17 15:03:32 -0600

    Added CVSS (v2/v3) calculator API resource. Accepts v2 or v3 vectors as input and returns base, impact, and exploitability scores.

---
+++ src/main/java/org/owasp/dependencytrack/resources/v1/CalculatorResource.java
@@ -0,0 +1,77 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) Steve Springett. All Rights Reserved.
+ */
+package org.owasp.dependencytrack.resources.v1;
+
+import alpine.resources.AlpineResource;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
+import org.apache.commons.lang.StringUtils;
+import us.springett.cvss.Cvss;
+import us.springett.cvss.Score;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * JAX-RS resources for processing severity calculations.
+ *
+ * @author Steve Springett
+ * @since 3.0.0
+ */
+@Path("/v1/calculator")
+@Api(value = "calculator", authorizations = @Authorization(value = "X-Api-Key"))
+public class CalculatorResource extends AlpineResource {
+
+    @GET
+    @Path("/cvss")
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(
+            value = "Returns the CVSS base score, impact sub-score and exploitability sub-score",
+            response = Score.class
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 401, message = "Unauthorized")
+    })
+    public Response getCvssScores(
+            @ApiParam(value = "A valid CVSSv2 or CVSSv3 vector", required = true)
+            @QueryParam("vector") String vector) {
+
+        final String invalidVector = "An invalid CVSSv2 or CVSSv3 vector submitted.";
+        if (StringUtils.trimToNull(vector) == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(invalidVector).build();
+        }
+        Cvss cvss = Cvss.fromVector(vector);
+        if (cvss == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(invalidVector).build();
+        }
+        Score score = cvss.calculateScore();
+        if (score == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(invalidVector).build();
+        } else {
+            return Response.ok(score).build();
+        }
+    }
+
+}

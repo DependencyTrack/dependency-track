7ac53b3b9230e3ff2b4eb2bcebf8f5ba077157cc
Steve Springett <steve@springett.us>
2018-02-27 21:39:18 -0600

    Codacy fixes

--- src/main/java/org/owasp/dependencytrack/search/IndexConstants.java
+++ src/main/java/org/owasp/dependencytrack/search/IndexConstants.java
@@ -27,11 +27,6 @@ import java.util.HashMap;
  */
 public final class IndexConstants {
 
-    /**
-     * Private constructor.
-     */
-    private IndexConstants() { }
-
     static final String PROJECT_UUID = "uuid";
     static final String PROJECT_NAME = "name";
     static final String PROJECT_VERSION = "version";
@@ -66,34 +61,6 @@ public final class IndexConstants {
             LICENSE_UUID, LICENSE_LICENSEID, LICENSE_NAME
     };
 
-    private static HashMap<String, Float> searchBoosts = new HashMap<>();
-
-    public static HashMap<String, Float> getBoostMap() {
-        if (searchBoosts.isEmpty()) {
-            searchBoosts.put(PROJECT_UUID, BOOST_PROJECT_UUID);
-            searchBoosts.put(PROJECT_NAME, BOOST_PROJECT_NAME);
-            searchBoosts.put(PROJECT_VERSION, BOOST_PROJECT_VERSION);
-            searchBoosts.put(PROJECT_PROPERTIES, BOOST_PROJECT_PROPERTIESC);
-            searchBoosts.put(PROJECT_DESCRIPTION, BOOST_PROJECT_DESCRIPTION);
-
-            searchBoosts.put(COMPONENT_UUID, BOOST_COMPONENT_UUID);
-            searchBoosts.put(COMPONENT_NAME, BOOST_COMPONENT_NAME);
-            searchBoosts.put(COMPONENT_GROUP, BOOST_COMPONENT_GROUP);
-            searchBoosts.put(COMPONENT_VERSION, BOOST_COMPONENT_VERSION);
-            searchBoosts.put(COMPONENT_SHA1, BOOST_COMPONENT_SHA1);
-            searchBoosts.put(COMPONENT_DESCRIPTION, BOOST_COMPONENT_DESCRIPTION);
-
-            searchBoosts.put(VULNERABILITY_UUID, BOOST_VULNERABILITY_UUID);
-            searchBoosts.put(VULNERABILITY_VULNID, BOOST_VULNERABILITY_VULNID);
-            searchBoosts.put(VULNERABILITY_DESCRIPTION, BOOST_VULNERABILITY_DESCRIPTION);
-
-            searchBoosts.put(LICENSE_UUID, BOOST_LICENSE_UUID);
-            searchBoosts.put(LICENSE_LICENSEID, BOOST_LICENSE_LICENSEID);
-            searchBoosts.put(LICENSE_NAME, BOOST_LICENSE_NAME);
-        }
-        return searchBoosts;
-    }
-
     /**
      * Field constants highlighting boost factors in performing search
      * for various user-defined fields.
@@ -121,4 +88,37 @@ public final class IndexConstants {
     private static final float BOOST_LICENSE_UUID = 100.0f;
     private static final float BOOST_LICENSE_LICENSEID = 90.0f;
     private static final float BOOST_LICENSE_NAME = 20.0f;
+
+    /**
+     * Private constructor.
+     */
+    private IndexConstants() { }
+
+    private static HashMap<String, Float> searchBoosts = new HashMap<>();
+
+    public static HashMap<String, Float> getBoostMap() {
+        if (searchBoosts.isEmpty()) {
+            searchBoosts.put(PROJECT_UUID, BOOST_PROJECT_UUID);
+            searchBoosts.put(PROJECT_NAME, BOOST_PROJECT_NAME);
+            searchBoosts.put(PROJECT_VERSION, BOOST_PROJECT_VERSION);
+            searchBoosts.put(PROJECT_PROPERTIES, BOOST_PROJECT_PROPERTIESC);
+            searchBoosts.put(PROJECT_DESCRIPTION, BOOST_PROJECT_DESCRIPTION);
+
+            searchBoosts.put(COMPONENT_UUID, BOOST_COMPONENT_UUID);
+            searchBoosts.put(COMPONENT_NAME, BOOST_COMPONENT_NAME);
+            searchBoosts.put(COMPONENT_GROUP, BOOST_COMPONENT_GROUP);
+            searchBoosts.put(COMPONENT_VERSION, BOOST_COMPONENT_VERSION);
+            searchBoosts.put(COMPONENT_SHA1, BOOST_COMPONENT_SHA1);
+            searchBoosts.put(COMPONENT_DESCRIPTION, BOOST_COMPONENT_DESCRIPTION);
+
+            searchBoosts.put(VULNERABILITY_UUID, BOOST_VULNERABILITY_UUID);
+            searchBoosts.put(VULNERABILITY_VULNID, BOOST_VULNERABILITY_VULNID);
+            searchBoosts.put(VULNERABILITY_DESCRIPTION, BOOST_VULNERABILITY_DESCRIPTION);
+
+            searchBoosts.put(LICENSE_UUID, BOOST_LICENSE_UUID);
+            searchBoosts.put(LICENSE_LICENSEID, BOOST_LICENSE_LICENSEID);
+            searchBoosts.put(LICENSE_NAME, BOOST_LICENSE_NAME);
+        }
+        return searchBoosts;
+    }
 }

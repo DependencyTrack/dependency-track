d89360c024a25c829deabc79e8872ec424281caa
Steve Springett <steve@springett.us>
2018-03-09 13:15:31 -0600

    Correcting purl

--- src/test/java/org/owasp/dependencytrack/parser/cyclonedx/CycloneDxParserTest.java
+++ src/test/java/org/owasp/dependencytrack/parser/cyclonedx/CycloneDxParserTest.java
@@ -58,7 +58,7 @@ public class CycloneDxParserTest extends BaseTest {
             Assert.assertEquals("708f1f53b41f11f02d12a11b1a38d2905d47b099afc71a0f1124ef8582ec7313", c1.getSha256());
             Assert.assertEquals("387b7ae16b9cae45f830671541539bf544202faae5aac544a93b7b0a04f5f846fa2f4e81ef3f1677e13aed7496408a441f5657ab6d54423e56bf6f38da124aef", c1.getSha512());
             Assert.assertEquals("cpe:/a:example:myapplication:1.0.0", c1.getCpe());
-            Assert.assertEquals("pkg:maven/com.example/myapplication@1.0.0packaging=war", c1.getPurl().canonicalize());
+            Assert.assertEquals("pkg:maven/com.example/myapplication@1.0.0?packaging=war", c1.getPurl().canonicalize());
             Assert.assertEquals("An example application", c1.getDescription());
             Assert.assertEquals("Copyright Example Inc. All rights reserved.", c1.getCopyright());
             Assert.assertEquals("Apache-2.0", c1.getResolvedLicense().getLicenseId());

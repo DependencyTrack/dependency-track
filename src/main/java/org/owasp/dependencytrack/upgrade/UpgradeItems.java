add4fc38223cf615c31da0caa30cfb5db1993160
Steve Springett <steve@springett.us>
2018-06-18 11:59:24 -0500

    Added upgrade initializer and default 3.1.0 upgrader to store the current schema

---
+++ src/main/java/org/owasp/dependencytrack/upgrade/UpgradeItems.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) Steve Springett. All Rights Reserved.
+ */
+package org.owasp.dependencytrack.upgrade;
+
+import alpine.upgrade.UpgradeItem;
+import org.owasp.dependencytrack.upgrade.upgraders.ODT310;
+import java.util.ArrayList;
+import java.util.List;
+
+class UpgradeItems {
+
+    private static final List<Class<? extends UpgradeItem>> UPGRADE_ITEMS = new ArrayList<>();
+    static {
+        UPGRADE_ITEMS.add(ODT310.class);
+    }
+
+    static List<Class<? extends UpgradeItem>> getUpgradeItems() {
+        return UPGRADE_ITEMS;
+    }
+
+}

1fb80ea1fd127dc0d7cb8f2bcf28159d6308fed5
Steve Springett <steve@springett.us>
2019-05-06 00:32:05 -0500

    Updated header

--- prerelease.sh
+++ prerelease.sh
@@ -4,7 +4,7 @@
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 export PATH=JAVA_HOME/bin:$PATH
 
-mvn clean package
+mvn clean package -Dmaven.test.skip=true
 mvn package -Dmaven.test.skip=true -P embedded-jetty -Dlogback.configuration.file=src/main/docker/logback.xml
 mvn net.nicoulaj.maven.plugins:checksum-maven-plugin:files
 mvn github-release:release

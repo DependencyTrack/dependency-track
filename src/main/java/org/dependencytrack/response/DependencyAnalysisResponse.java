adc346578a276c5bd70c5cbc8c5028d7bfc29c17
mathuriga <tmathu24@yahoo.com>
2019-06-06 12:10:50 +0530

    Add DependencyAnalysisResponse.java

---
+++ src/main/java/org/dependencytrack/response/DependencyAnalysisResponse.java
@@ -0,0 +1,104 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Steve Springett. All Rights Reserved.
+ */
+package org.dependencytrack.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.dependencytrack.model.AnalysisState;
+import java.util.UUID;
+
+/**
+ * Defines a custom response object used when getting the analysis comment per component.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DependencyAnalysisResponse {
+    private String name;
+    private String version;
+    private UUID uuid;
+    private String componentName;
+    private String lastComment;
+    private AnalysisState analysisState;
+    private boolean suppressed;
+
+    public DependencyAnalysisResponse(String name, String version, UUID uuid, String componentName, String lastComment,
+            AnalysisState analysisState, boolean suppressed) {
+        this.name = name;
+        this.version = version;
+        this.uuid = uuid;
+        this.componentName = componentName;
+        this.lastComment = lastComment;
+        this.analysisState = analysisState;
+        this.suppressed = suppressed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getLastComment() {
+        return lastComment;
+    }
+
+    public void setLastComment(String lastComment) {
+        this.lastComment = lastComment;
+    }
+
+    public AnalysisState getAnalysisState() {
+        return analysisState;
+    }
+
+    public void setAnalysisState(AnalysisState analysisState) {
+        this.analysisState = analysisState;
+    }
+
+    public boolean isSuppressed() {
+        return suppressed;
+    }
+
+    public void setSuppressed(boolean suppressed) {
+        this.suppressed = suppressed;
+    }
+
+    public String getComponentName() {
+        return componentName;
+    }
+
+    public void setComponentName(String componentName) {
+        this.componentName = componentName;
+    }
+}

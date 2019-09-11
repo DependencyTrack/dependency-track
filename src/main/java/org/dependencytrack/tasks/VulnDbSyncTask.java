67aa2681f6beabf6fd07d7bc72e468deb386685f
Steve Springett <steve@springett.us>
2019-09-11 12:54:17 -0500

    #110 - Storing CPEs and VulnerableSoftware objects for each VulnDB ID so that the internal CPE analyzer can be used to discover vulns from VulnDB

--- src/main/java/org/dependencytrack/tasks/VulnDbSyncTask.java
+++ src/main/java/org/dependencytrack/tasks/VulnDbSyncTask.java
@@ -27,15 +27,26 @@ import alpine.notification.NotificationLevel;
 import org.dependencytrack.event.IndexEvent;
 import org.dependencytrack.event.VulnDbSyncEvent;
 import org.dependencytrack.model.Vulnerability;
+import org.dependencytrack.model.VulnerableSoftware;
 import org.dependencytrack.notification.NotificationConstants;
 import org.dependencytrack.notification.NotificationGroup;
 import org.dependencytrack.notification.NotificationScope;
-import org.dependencytrack.parser.vulndb.util.ModelConverter;
+import org.dependencytrack.parser.vulndb.ModelConverter;
 import org.dependencytrack.persistence.QueryManager;
+import us.springett.parsers.cpe.Cpe;
+import us.springett.parsers.cpe.CpeParser;
+import us.springett.parsers.cpe.exceptions.CpeEncodingException;
+import us.springett.parsers.cpe.exceptions.CpeParsingException;
 import us.springett.vulndbdatamirror.parser.VulnDbParser;
+import us.springett.vulndbdatamirror.parser.model.CPE;
+import us.springett.vulndbdatamirror.parser.model.Product;
 import us.springett.vulndbdatamirror.parser.model.Results;
+import us.springett.vulndbdatamirror.parser.model.Vendor;
+import us.springett.vulndbdatamirror.parser.model.Version;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -113,10 +124,74 @@ public class VulnDbSyncTask implements LoggableSubscriber {
                 if (o instanceof us.springett.vulndbdatamirror.parser.model.Vulnerability) {
                     final us.springett.vulndbdatamirror.parser.model.Vulnerability vulnDbVuln = (us.springett.vulndbdatamirror.parser.model.Vulnerability)o;
                     final org.dependencytrack.model.Vulnerability vulnerability = ModelConverter.convert(qm, vulnDbVuln);
-                    qm.synchronizeVulnerability(vulnerability, false);
+                    final Vulnerability synchronizeVulnerability = qm.synchronizeVulnerability(vulnerability, false);
+                    final List<VulnerableSoftware> vsList = parseCpes(qm, synchronizeVulnerability, vulnDbVuln);
+                    synchronizeVulnerability.setVulnerableSoftware(vsList);
+                    qm.persist(synchronizeVulnerability);
                 }
             }
         }
+    }
+
+    public static List<VulnerableSoftware> parseCpes(final QueryManager qm, final Vulnerability vulnerability,
+                                                     final us.springett.vulndbdatamirror.parser.model.Vulnerability vulnDbVuln) {
+        // cpe:2.3:a:belavier_commerce:abantecart:1.2.8:*:*:*:*:*:*:*
+        final List<VulnerableSoftware> vsList = new ArrayList<>();
+        if (vulnDbVuln.getVendors() != null) {
+            for (Vendor vendor: vulnDbVuln.getVendors()) {
+                if (vendor.getProducts() != null) {
+                    for (Product product: vendor.getProducts()) {
+                        if (product.getVersions() != null) {
+                            for (Version version: product.getVersions()) {
+                                if (version != null) {
+                                    if (version.isAffected()) {
+                                        if (version.getCpes() != null) {
+                                            for (CPE cpeObject : version.getCpes()) {
+                                                try {
+                                                    final Cpe cpe = CpeParser.parse(cpeObject.getCpe(), true);
+                                                    final VulnerableSoftware vs = generateVulnerableSoftware(qm, cpe, vulnerability);
+                                                    if (vs != null) {
+                                                        vsList.add(vs);
+                                                    }
+                                                } catch (CpeParsingException e) {
+                                                    // Normally, this would be logged to error, however, VulnDB contains a lot of invalid CPEs
+                                                    LOGGER.debug("An error occurred parsing " + cpeObject.getCpe(), e);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return vsList;
+    }
+
+    private static VulnerableSoftware generateVulnerableSoftware(final QueryManager qm, final Cpe cpe,
+                                                                 final Vulnerability vulnerability) {
+        VulnerableSoftware vs = qm.getVulnerableSoftwareByCpe23(cpe.toCpe23FS(), null, null, null, null);
+        if (vs != null) {
+            return vs;
+        }
+        try {
+            vs = org.dependencytrack.parser.nvd.ModelConverter.convertCpe23UriToVulnerableSoftware(cpe.toCpe23FS());
+            vs.setVulnerable(true);
+            vs.addVulnerability(vulnerability);
+            // VulnDB does not provide version ranges for the CPEs that exist inside Vendor->Product->Version->CPE
+            vs.setVersionEndExcluding(null);
+            vs.setVersionEndIncluding(null);
+            vs.setVersionStartExcluding(null);
+            vs.setVersionStartIncluding(null);
+            vs = qm.persist(vs);
+            //Event.dispatch(new IndexEvent(IndexEvent.Action.CREATE, qm.detach(VulnerableSoftware.class, vs.getId())));
+            return vs;
+        } catch (CpeParsingException | CpeEncodingException e) {
+            LOGGER.error("An error occurred while parsing: " + cpe.toCpe23FS(), e);
+        }
+        return null;
     }
 
 }

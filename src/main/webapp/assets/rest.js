30d1abd6eb79f7147e88c5b1e192120cc96c7a7b
Steve Springett <steve@springett.us>
2019-12-13 16:19:10 -0600

    #469 - Fixed encoding issue

--- src/main/webapp/assets/rest.js
+++ src/main/webapp/assets/rest.js
@@ -387,8 +387,7 @@ $rest.getProject = function getProject(uuid, successCallback, failCallback) {
  * Service called to retrieve a list of all projects with the specified name
  */
 $rest.getProjectVersions = function getProjectVersions(name, excludeInactive, successCallback, failCallback) {
-    let queryString = (excludeInactive) ? "name=" + name + "&excludeInactive=true" : "name=" + name;
-    console.log(queryString);
+    let queryString = (excludeInactive) ? "name=" + encodeURIComponent(name) + "&excludeInactive=true" : "name=" + encodeURIComponent(name);
     $.ajax({
         url: $rest.contextPath() + URL_PROJECT + "?" + queryString,
         contentType: CONTENT_TYPE_JSON,

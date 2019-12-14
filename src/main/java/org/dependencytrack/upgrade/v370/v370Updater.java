d90e604bc5c7551cd7ccf0b30fbd615f07064808
Steve Springett <steve@springett.us>
2019-12-14 16:52:46 -0600

    Adding deletion of scan data and drop of scan table. Reordered

--- src/main/java/org/dependencytrack/upgrade/v370/v370Updater.java
+++ src/main/java/org/dependencytrack/upgrade/v370/v370Updater.java
@@ -29,16 +29,17 @@ import java.sql.Statement;
 public class v370Updater extends AbstractUpgradeItem {
 
     private static final Logger LOGGER = Logger.getLogger(v370Updater.class);
-    private static final String STMT_1 = "SELECT \"ID\" FROM \"PERMISSION\" WHERE \"NAME\" = 'SCAN_UPLOAD'";
-    private static final String STMT_2 = "DELETE FROM \"TEAMS_PERMISSIONS\" WHERE \"PERMISSION_ID\" = %d";
-    private static final String STMT_3 = "DELETE FROM \"LDAPUSERS_PERMISSIONS\" WHERE \"PERMISSION_ID\" = %d";
-    private static final String STMT_4 = "DELETE FROM \"MANAGEDUSERS_PERMISSIONS\" WHERE \"PERMISSION_ID\" = %d";
-    private static final String STMT_5 = "DELETE FROM \"PERMISSION\" WHERE \"ID\" = %d";
-    private static final String STMT_6 = "DELETE FROM \"SCANS_COMPONENTS\"";
-    private static final String STMT_7 = "UPDATE \"PROJECT\" SET \"LAST_SCAN_IMPORTED\" = NULL";
-    private static final String STMT_8 = "DELETE FROM \"CONFIGPROPERTY\" WHERE \"GROUPNAME\" = 'artifact' AND \"PROPERTYNAME\" = 'dependencycheck.enabled'";
-    private static final String STMT_9 = "UPDATE \"COMPONENT\" SET \"INTERNAL\" = FALSE WHERE \"INTERNAL\" IS NULL";
-    private static final String STMT_9_ALT = "UPDATE \"COMPONENT\" SET \"INTERNAL\" = 0 WHERE \"INTERNAL\" IS NULL";
+    private static final String STMT_1 = "UPDATE \"COMPONENT\" SET \"INTERNAL\" = FALSE WHERE \"INTERNAL\" IS NULL";
+    private static final String STMT_1_ALT = "UPDATE \"COMPONENT\" SET \"INTERNAL\" = 0 WHERE \"INTERNAL\" IS NULL";
+    private static final String STMT_2 = "SELECT \"ID\" FROM \"PERMISSION\" WHERE \"NAME\" = 'SCAN_UPLOAD'";
+    private static final String STMT_3 = "DELETE FROM \"TEAMS_PERMISSIONS\" WHERE \"PERMISSION_ID\" = %d";
+    private static final String STMT_4 = "DELETE FROM \"LDAPUSERS_PERMISSIONS\" WHERE \"PERMISSION_ID\" = %d";
+    private static final String STMT_5 = "DELETE FROM \"MANAGEDUSERS_PERMISSIONS\" WHERE \"PERMISSION_ID\" = %d";
+    private static final String STMT_6 = "DELETE FROM \"PERMISSION\" WHERE \"ID\" = %d";
+    private static final String STMT_7 = "DELETE FROM \"SCANS_COMPONENTS\"";
+    private static final String STMT_8 = "UPDATE \"PROJECT\" SET \"LAST_SCAN_IMPORTED\" = NULL";
+    private static final String STMT_9 = "DELETE FROM \"SCAN\"";
+    private static final String STMT_10 = "DELETE FROM \"CONFIGPROPERTY\" WHERE \"GROUPNAME\" = 'artifact' AND \"PROPERTYNAME\" = 'dependencycheck.enabled'";
 
     @Override
     public String getSchemaVersion() {
@@ -49,40 +50,46 @@ public class v370Updater extends AbstractUpgradeItem {
     public void executeUpgrade(final AlpineQueryManager alpineQueryManager, final Connection connection) throws Exception {
         LOGGER.info("Updating existing components to be non-internal");
         try {
-            DbUtil.executeUpdate(connection, STMT_9);
+            DbUtil.executeUpdate(connection, STMT_1);
         } catch (Exception e) {
             LOGGER.info("Internal field is likely not boolean. Attempting component internal status update assuming bit field");
-            DbUtil.executeUpdate(connection, STMT_9_ALT);
+            DbUtil.executeUpdate(connection, STMT_1_ALT);
         }
 
         LOGGER.info("Removing legacy SCAN_UPLOAD permission");
         final Statement q = connection.createStatement();
-        final ResultSet rs = q.executeQuery(STMT_1);
+        final ResultSet rs = q.executeQuery(STMT_2);
         while(rs.next()) {
             final long id = rs.getLong(1);
             LOGGER.info("Removing SCAN_UPLOAD from the TEAMS_PERMISSIONS table");
-            DbUtil.executeUpdate(connection, String.format(STMT_2, id));
-            LOGGER.info("Removing SCAN_UPLOAD from the LDAPUSERS_PERMISSIONS table");
             DbUtil.executeUpdate(connection, String.format(STMT_3, id));
-            LOGGER.info("Removing SCAN_UPLOAD from the MANAGEDUSERS_PERMISSIONS table");
+            LOGGER.info("Removing SCAN_UPLOAD from the LDAPUSERS_PERMISSIONS table");
             DbUtil.executeUpdate(connection, String.format(STMT_4, id));
-            LOGGER.info("Removing SCAN_UPLOAD from the PERMISSION table");
+            LOGGER.info("Removing SCAN_UPLOAD from the MANAGEDUSERS_PERMISSIONS table");
             DbUtil.executeUpdate(connection, String.format(STMT_5, id));
+            LOGGER.info("Removing SCAN_UPLOAD from the PERMISSION table");
+            DbUtil.executeUpdate(connection, String.format(STMT_6, id));
         }
 
         LOGGER.info("Removing legacy SCANS_COMPONENTS data");
-        DbUtil.executeUpdate(connection, STMT_6);
-
-        LOGGER.info("Removing legacy LAST_SCAN_IMPORTED project dates");
         DbUtil.executeUpdate(connection, STMT_7);
 
-        LOGGER.info("Removing legacy Dependency-Check configuration settings");
+        LOGGER.info("Removing legacy LAST_SCAN_IMPORTED project dates");
         DbUtil.executeUpdate(connection, STMT_8);
+
+        LOGGER.info("Removing legacy SCAN dates");
+        DbUtil.executeUpdate(connection, STMT_9);
+
+        LOGGER.info("Removing legacy Dependency-Check configuration settings");
+        DbUtil.executeUpdate(connection, STMT_10);
 
         LOGGER.info("Dropping SCANS_COMPONENTS table");
         DbUtil.dropTable(connection, "SCANS_COMPONENTS");
 
         LOGGER.info("Dropping LAST_SCAN_IMPORTED column from PROJECT table");
         DbUtil.dropColumn(connection, "PROJECT", "LAST_SCAN_IMPORTED");
+
+        LOGGER.info("Dropping SCANS_COMPONENTS table");
+        DbUtil.dropTable(connection, "SCAN");
     }
 }

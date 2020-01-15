5ad69fb6fb331f06d04ae6df147eca54a1535b10
Steve Springett <steve@springett.us>
2020-01-15 16:24:40 -0600

    Adding Maven Central SQL fix

---
+++ upgrade/3.7.1_and_previous_maven_central_fix.sql
@@ -0,0 +1,8 @@
+/*
+This statement changes the URL for Maven Central from using HTTP to HTTPS.
+As of January 2020, Maven Central no longer accepts HTTP requests and will
+respond with a 501. This prevents Java components from being analyzed for
+being out-of-date.
+*/
+
+UPDATE "REPOSITORY" SET "URL" = "https://central.maven.org/maven2/" WHERE "TYPE" = "MAVEN" AND "IDENTIFIER" = "central";

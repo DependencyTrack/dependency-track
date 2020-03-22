61d8c73855b3c7db370b21f1ecdcbe732a10fbad
Steve Springett <steve@springett.us>
2020-03-22 13:51:17 -0500

    Optimized imports

--- src/main/java/org/dependencytrack/model/Repository.java
+++ src/main/java/org/dependencytrack/model/Repository.java
@@ -22,8 +22,13 @@ import alpine.json.TrimmedStringDeserializer;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-
-import javax.jdo.annotations.*;
+import javax.jdo.annotations.Column;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.Index;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+import javax.jdo.annotations.Unique;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;

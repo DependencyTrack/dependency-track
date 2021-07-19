0d35e5d2c3bc50ce618f68cd5ec0b0fd71b00418
nscuro <nscuro@protonmail.com>
2021-07-19 22:15:08 +0200

    Fix tests
    Failures were a result of https://github.com/DependencyTrack/dependency-track/pull/1110
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/test/java/org/dependencytrack/persistence/DefaultObjectGeneratorTest.java
+++ src/test/java/org/dependencytrack/persistence/DefaultObjectGeneratorTest.java
@@ -68,7 +68,7 @@ public class DefaultObjectGeneratorTest extends PersistenceCapableTest {
         Method method = generator.getClass().getDeclaredMethod("loadDefaultRepositories");
         method.setAccessible(true);
         method.invoke(generator);
-        Assert.assertEquals(12, qm.getAllRepositories().size());
+        Assert.assertEquals(13, qm.getAllRepositories().size());
     }
 
     @Test

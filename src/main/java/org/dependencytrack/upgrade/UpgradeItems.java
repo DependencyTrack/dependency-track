7d1b17a81636c1b8df1d562fe23c2e380c6d5c19
nscuro <nscuro@protonmail.com>
2022-05-10 18:19:23 +0200

    Enable updater for v4.5.0
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/main/java/org/dependencytrack/upgrade/UpgradeItems.java
+++ src/main/java/org/dependencytrack/upgrade/UpgradeItems.java
@@ -31,6 +31,7 @@ class UpgradeItems {
         UPGRADE_ITEMS.add(org.dependencytrack.upgrade.v410.v410Updater.class);
         UPGRADE_ITEMS.add(org.dependencytrack.upgrade.v420.v420Updater.class);
         UPGRADE_ITEMS.add(org.dependencytrack.upgrade.v440.v440Updater.class);
+        UPGRADE_ITEMS.add(org.dependencytrack.upgrade.v450.v450Updater.class);
     }
 
     static List<Class<? extends UpgradeItem>> getUpgradeItems() {

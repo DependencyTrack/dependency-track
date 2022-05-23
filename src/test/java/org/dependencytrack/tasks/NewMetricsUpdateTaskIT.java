fe6a687b27949dc2557fe236ac99ce875a7c106d
nscuro <nscuro@protonmail.com>
2022-05-23 19:46:31 +0200

    Fill NewMetricsUpdateTaskIT with data
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/test/java/org/dependencytrack/tasks/NewMetricsUpdateTaskIT.java
+++ src/test/java/org/dependencytrack/tasks/NewMetricsUpdateTaskIT.java
@@ -5,28 +5,40 @@ import alpine.server.persistence.PersistenceManagerFactory;
 import org.datanucleus.PropertyNames;
 import org.datanucleus.api.jdo.JDOPersistenceManagerFactory;
 import org.dependencytrack.event.MetricsUpdateEvent;
+import org.dependencytrack.model.AnalysisState;
+import org.dependencytrack.model.Component;
+import org.dependencytrack.model.Policy;
+import org.dependencytrack.model.PolicyCondition;
+import org.dependencytrack.model.PolicyViolation;
 import org.dependencytrack.model.PortfolioMetrics;
 import org.dependencytrack.model.Project;
+import org.dependencytrack.model.Severity;
+import org.dependencytrack.model.ViolationAnalysisState;
+import org.dependencytrack.model.Vulnerability;
 import org.dependencytrack.persistence.QueryManager;
+import org.dependencytrack.tasks.scanners.AnalyzerIdentity;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.testcontainers.containers.Container;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy;
 import org.testcontainers.utility.DockerImageName;
 
 import javax.jdo.JDOHelper;
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NewMetricsUpdateTaskIT {
 
     @Ignore // There is no image for arm64
-    public static class MssqlTest {
+    public static class MssqlIT {
 
         @Rule
         @SuppressWarnings("rawtypes")
@@ -39,7 +51,8 @@ public class NewMetricsUpdateTaskIT {
         @Before
         public void setUp() throws Exception {
             // We need to create the database manually because the container won't do it automatically.
-            container.execInContainer("/opt/mssql-tools/bin/sqlcmd", "-S", "localhost", "-U", "sa", "-P", "DTrack1234!", "-Q", "CREATE DATABASE dtrack");
+            final Container.ExecResult execResult = container.execInContainer("/opt/mssql-tools/bin/sqlcmd", "-S", "localhost", "-U", "sa", "-P", "DTrack1234!", "-Q", "CREATE DATABASE dtrack");
+            assertThat(execResult.getExitCode()).isZero();
 
             final Properties jdoProps = JdoProperties.get();
             jdoProps.setProperty(PropertyNames.PROPERTY_CONNECTION_URL, "jdbc:sqlserver://localhost:" + container.getFirstMappedPort() +
@@ -59,9 +72,7 @@ public class NewMetricsUpdateTaskIT {
             new NewMetricsUpdateTask().inform(new MetricsUpdateEvent(MetricsUpdateEvent.Type.PORTFOLIO));
 
             try (final var qm = new QueryManager()) {
-                final PortfolioMetrics metrics = qm.getMostRecentPortfolioMetrics();
-                assertThat(metrics.getProjects()).isEqualTo(1);
-                assertThat(metrics.getComponents()).isZero();
+                assertResult(qm.getMostRecentPortfolioMetrics());
             }
         }
 
@@ -72,7 +83,7 @@ public class NewMetricsUpdateTaskIT {
 
     }
 
-    public static class PostgresTest {
+    public static class PostgresIT {
 
         @Rule
         @SuppressWarnings("rawtypes")
@@ -102,9 +113,7 @@ public class NewMetricsUpdateTaskIT {
             new NewMetricsUpdateTask().inform(new MetricsUpdateEvent(MetricsUpdateEvent.Type.PORTFOLIO));
 
             try (final var qm = new QueryManager()) {
-                final PortfolioMetrics metrics = qm.getMostRecentPortfolioMetrics();
-                assertThat(metrics.getProjects()).isEqualTo(1);
-                assertThat(metrics.getComponents()).isZero();
+                assertResult(qm.getMostRecentPortfolioMetrics());
             }
         }
 
@@ -117,12 +126,68 @@ public class NewMetricsUpdateTaskIT {
 
     protected static void populateTestData() {
         try (final var qm = new QueryManager()) {
-            // TODO: Add enough data to cover all queries in the update task
-
             var project = new Project();
             project.setName("acme-app");
             qm.createProject(project, List.of(), false);
+
+            var component = new Component();
+            component.setProject(project);
+            component.setName("acme-lib");
+            component = qm.createComponent(component, false);
+
+
+            var vuln = new Vulnerability();
+            vuln.setVulnId("INTERNAL-001");
+            vuln.setSource(Vulnerability.Source.INTERNAL);
+            vuln.setSeverity(Severity.HIGH);
+            vuln = qm.createVulnerability(vuln, false);
+            qm.addVulnerability(vuln, component, AnalyzerIdentity.NONE);
+            qm.makeAnalysis(component, vuln, AnalysisState.NOT_AFFECTED, null, null, null, false);
+
+            final var policy = qm.createPolicy(UUID.randomUUID().toString(), Policy.Operator.ALL, Policy.ViolationState.FAIL);
+            final var policyCondition = qm.createPolicyCondition(policy, PolicyCondition.Subject.COORDINATES, PolicyCondition.Operator.MATCHES, "");
+            var policyViolation = new PolicyViolation();
+            policyViolation.setComponent(component);
+            policyViolation.setPolicyCondition(policyCondition);
+            policyViolation.setTimestamp(new Date());
+            policyViolation.setType(PolicyViolation.Type.OPERATIONAL);
+            policyViolation = qm.addPolicyViolationIfNotExist(policyViolation);
+            qm.makeViolationAnalysis(component, policyViolation, ViolationAnalysisState.APPROVED, false);
         }
+    }
+
+    protected static void assertResult(final PortfolioMetrics metrics) {
+        assertThat(metrics).isNotNull();
+        assertThat(metrics.getProjects()).isEqualTo(1);
+        assertThat(metrics.getVulnerableProjects()).isEqualTo(1);
+        assertThat(metrics.getComponents()).isEqualTo(1);
+        assertThat(metrics.getVulnerableComponents()).isEqualTo(1);
+        assertThat(metrics.getCritical()).isZero();
+        assertThat(metrics.getHigh()).isEqualTo(1);
+        assertThat(metrics.getMedium()).isZero();
+        assertThat(metrics.getLow()).isZero();
+        assertThat(metrics.getUnassigned()).isZero();
+        assertThat(metrics.getVulnerabilities()).isEqualTo(1);
+        assertThat(metrics.getSuppressed()).isZero();
+        assertThat(metrics.getFindingsTotal()).isEqualTo(1);
+        assertThat(metrics.getFindingsAudited()).isEqualTo(1);
+        assertThat(metrics.getFindingsUnaudited()).isZero();
+        assertThat(metrics.getInheritedRiskScore()).isEqualTo(5.0);
+        assertThat(metrics.getPolicyViolationsFail()).isEqualTo(1);
+        assertThat(metrics.getPolicyViolationsWarn()).isZero();
+        assertThat(metrics.getPolicyViolationsInfo()).isZero();
+        assertThat(metrics.getPolicyViolationsTotal()).isEqualTo(1);
+        assertThat(metrics.getPolicyViolationsAudited()).isEqualTo(1);
+        assertThat(metrics.getPolicyViolationsUnaudited()).isEqualTo(0);
+        assertThat(metrics.getPolicyViolationsSecurityTotal()).isZero();
+        assertThat(metrics.getPolicyViolationsSecurityAudited()).isZero();
+        assertThat(metrics.getPolicyViolationsSecurityUnaudited()).isZero();
+        assertThat(metrics.getPolicyViolationsLicenseTotal()).isZero();
+        assertThat(metrics.getPolicyViolationsLicenseAudited()).isZero();
+        assertThat(metrics.getPolicyViolationsLicenseUnaudited()).isZero();
+        assertThat(metrics.getPolicyViolationsOperationalTotal()).isEqualTo(1);
+        assertThat(metrics.getPolicyViolationsOperationalAudited()).isEqualTo(1);
+        assertThat(metrics.getPolicyViolationsOperationalUnaudited()).isZero();
     }
 
 }

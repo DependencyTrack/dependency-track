8087294a1a0071bc150078754d66181a7a02ec67
nscuro <nscuro@protonmail.com>
2022-05-29 21:29:47 +0200

    Remove outdated `upgrade` directory
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- upgrade/3.7.1_and_previous_maven_central_fix.sql
+++
@@ -1,8 +0,0 @@
-/*
-This statement changes the URL for Maven Central from using HTTP to HTTPS.
-As of January 2020, Maven Central no longer accepts HTTP requests and will
-respond with a 501. This prevents Java components from being analyzed for
-being out-of-date.
-*/
-
-UPDATE "REPOSITORY" SET "URL" = 'https://repo1.maven.org/maven2/' WHERE "TYPE" = 'MAVEN' AND "IDENTIFIER" = 'central';

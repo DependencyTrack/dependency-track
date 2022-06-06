78359a3db5a26f4c1977bbcca2fa3c567375c0de
Torsten Mehnert <torsten.mhn@gmail.com>
2022-06-06 10:18:45 +0200

    Add tests for Badger
    Signed-off-by: Torsten Mehnert <torsten.mhn@gmail.com>

---
+++ src/test/java/org/dependencytrack/resources/v1/misc/BadgerTest.java
@@ -0,0 +1,82 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Steve Springett. All Rights Reserved.
+ */
+package org.dependencytrack.resources.v1.misc;
+
+import org.apache.commons.io.FileUtils;
+import org.dependencytrack.model.ProjectMetrics;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class BadgerTest {
+
+    @Test
+    public void generateWithoutMetricsGenerateExpectedSvg() {
+        Badger badger = new Badger();
+        String svg = badger.generate(null);
+        Assert.assertEquals(strip(svg), strip(expectedSvg("vulnerabilities.nometrics.svg")));
+    }
+
+    @Test
+    public void generateWithoutVulnerabilitiesGenerateExpectedSvg() {
+        ProjectMetrics metrics = new ProjectMetrics();
+        metrics.setVulnerabilities(0);
+        Badger badger = new Badger();
+        String svg = badger.generate(metrics);
+        Assert.assertEquals(strip(svg), strip(expectedSvg("vulnerabilities.novulns.svg")));
+    }
+
+    @Test
+    public void generateWithVulnerabilitiesGenerateExpectedSvg() {
+        ProjectMetrics metrics = new ProjectMetrics();
+        metrics.setVulnerabilities(1 + 2 + 3 + 4 + 5);
+        metrics.setCritical(1);
+        metrics.setHigh(2);
+        metrics.setMedium(3);
+        metrics.setLow(4);
+        metrics.setUnassigned(5);
+        Badger badger = new Badger();
+        String svg = badger.generate(metrics);
+        Assert.assertEquals(strip(svg), strip(expectedSvg("vulnerabilities.vulns.svg")));
+    }
+
+    private String expectedSvg(String filename) {
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        URL resource = contextClassLoader.getResource("badge/" + filename);
+        if (resource == null) {
+            throw new RuntimeException("can't find expected svg filename=" + filename);
+        }
+        try {
+            return FileUtils.readFileToString(new File(resource.getFile()), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("can't load expected svg filename=" + filename);
+        }
+    }
+
+    private static String strip(String svg) {
+        return svg
+                .trim()
+                .replaceAll(" {2}", "")
+                .replaceAll("\r\n", "\n");
+    }
+}

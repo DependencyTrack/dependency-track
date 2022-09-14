e30b92b3b72e6b4924ab064b9c37455eea490e02
Sahiba Mittal <sahibamittal98@gmail.com>
2022-09-14 14:11:00 +0100

    Squashed commit of the following:
    commit 4b8e98fa1b9ac00971a7f4fc6d935d727ba706ac
    Merge: 60fc8678 7805a726
    Author: Niklas <nscuro@protonmail.com>
    Date:   Tue Sep 13 14:29:45 2022 +0200
    
        Merge pull request #1949 from nscuro/issue-1947
    
        Refactor `InternalComponentIdentificationTask` to use pagination
    
    commit 7805a726960701b383ba621f71ebeb7b64ed155f
    Author: nscuro <nscuro@protonmail.com>
    Date:   Mon Sep 12 22:26:33 2022 +0200
    
        Refactor `InternalComponentIdentificationTask` to use pagination
    
        Instead of loading all components at once. Also use a dedicated fetch group to limit the data being fetched. Remove unused code from `InternalComponentIdentificationEvent`.
    
        Signed-off-by: nscuro <nscuro@protonmail.com>
    
    commit 60fc8678b7ea205c6af50c3f0ffe60d2518f480f
    Merge: e9304da3 152945a4
    Author: Niklas <nscuro@protonmail.com>
    Date:   Sat Sep 10 00:33:02 2022 +0200
    
        Merge pull request #1942 from DependencyTrack/dependabot/maven/com.microsoft.sqlserver-mssql-jdbc-11.2.1.jre11
    
        Bump mssql-jdbc from 11.2.0.jre11 to 11.2.1.jre11
    
    commit 152945a4694c4e09535b5d9a8c84d6c7df897b96
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Fri Sep 9 08:02:30 2022 +0000
    
        Bump mssql-jdbc from 11.2.0.jre11 to 11.2.1.jre11
    
        Bumps [mssql-jdbc](https://github.com/Microsoft/mssql-jdbc) from 11.2.0.jre11 to 11.2.1.jre11.
        - [Release notes](https://github.com/Microsoft/mssql-jdbc/releases)
        - [Changelog](https://github.com/microsoft/mssql-jdbc/blob/main/CHANGELOG.md)
        - [Commits](https://github.com/Microsoft/mssql-jdbc/commits)
    
        ---
        updated-dependencies:
        - dependency-name: com.microsoft.sqlserver:mssql-jdbc
          dependency-type: direct:production
          update-type: version-update:semver-patch
        ...
    
        Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: Sahiba Mittal <sahibamittal98@gmail.com>

--- src/main/java/org/dependencytrack/tasks/InternalComponentIdentificationTask.java
+++ src/main/java/org/dependencytrack/tasks/InternalComponentIdentificationTask.java
@@ -21,11 +21,19 @@ package org.dependencytrack.tasks;
 import alpine.common.logging.Logger;
 import alpine.event.framework.Event;
 import alpine.event.framework.Subscriber;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.datanucleus.PropertyNames;
 import org.dependencytrack.event.InternalComponentIdentificationEvent;
 import org.dependencytrack.model.Component;
 import org.dependencytrack.persistence.QueryManager;
 import org.dependencytrack.util.InternalComponentIdentificationUtil;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import javax.jdo.Transaction;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 /**
@@ -40,38 +48,90 @@ public class InternalComponentIdentificationTask implements Subscriber {
 
     @Override
     public void inform(final Event e) {
-        if (!(e instanceof InternalComponentIdentificationEvent)) {
-            return;
+        if (e instanceof InternalComponentIdentificationEvent) {
+            LOGGER.info("Starting internal component identification");
+            final Instant startTime = Instant.now();
+            try {
+                analyze();
+            } catch (Exception ex) {
+                LOGGER.error("An unexpected error occurred while identifying internal components", ex);
+            }
+            LOGGER.info("Internal component identification completed in "
+                    + DateFormatUtils.format(Duration.between(startTime, Instant.now()).toMillis(), "mm:ss:SS"));
         }
-        final InternalComponentIdentificationEvent event = (InternalComponentIdentificationEvent)e;
-        LOGGER.info("Starting internal component identification task");
-        if (event.getComponents().size() > 0) {
-            analyze(event.getComponents());
-        } else {
-            analyze(null);
-        }
-        LOGGER.info("Internal component identification task completed");
     }
 
-    private void analyze(final List<Component> components) {
-        try (final QueryManager qm = new QueryManager()) {
-            for (final Component component : components != null ? components : qm.getAllComponents()) {
-                final boolean internal = InternalComponentIdentificationUtil.isInternalComponent(component, qm);
-                if (internal) {
-                    LOGGER.debug("Component " + component + " was identified to be internal");
-                }
-                if (component.isInternal() != internal) { // We want to log changes to a component's internal status.
+    private void analyze() throws Exception {
+        try (final var qm = new QueryManager()) {
+            final PersistenceManager pm = qm.getPersistenceManager();
+
+            // Disable the DataNucleus L2 cache for this persistence manager.
+            // The cache will hold references to the queried objects, preventing them
+            // from being garbage collected. This is not required the case of this task.
+            pm.setProperty(PropertyNames.PROPERTY_CACHE_L2_TYPE, "none");
+
+            List<Component> components = fetchNextComponentsPage(pm, null);
+            while (!components.isEmpty()) {
+                for (final Component component : components) {
+                    String coordinates = component.getName();
+                    if (StringUtils.isNotBlank(component.getGroup())) {
+                        coordinates = component.getGroup() + ":" + coordinates;
+                    }
+
+                    final boolean internal = InternalComponentIdentificationUtil.isInternalComponent(component, qm);
                     if (internal) {
-                        LOGGER.info("Component " + component + " was identified to be internal. It was previously not an internal component.");
-                    } else {
-                        LOGGER.info("Component " + component + " was previously identified as an internal component. It is no longer identified as internal.");
+                        LOGGER.debug("Component " + coordinates + " (" + component.getUuid() + ") was identified to be internal");
+                    }
+
+                    if (component.isInternal() != internal) {
+                        if (internal) {
+                            LOGGER.info("Component " + coordinates + " (" + component.getUuid()
+                                    + ") was identified to be internal. It was previously not an internal component.");
+                        } else {
+                            LOGGER.info("Component " + coordinates + " (" + component.getUuid()
+                                    + ") was previously identified as internal. It is no longer identified as internal.");
+                        }
+                    }
+
+                    if (component.isInternal() != internal) {
+                        final Transaction trx = pm.currentTransaction();
+                        try {
+                            trx.begin();
+                            component.setInternal(internal);
+                            trx.commit();
+                        } finally {
+                            if (trx.isActive()) {
+                                trx.rollback();
+                            }
+                        }
                     }
                 }
-                if (component.isInternal() != internal) {
-                    component.setInternal(internal);
-                    qm.persist(component);
-                }
+
+                final long lastId = components.get(components.size() - 1).getId();
+                components = fetchNextComponentsPage(pm, lastId);
             }
+        }
+    }
+
+    /**
+     * Efficiently page through all components using keyset pagination.
+     *
+     * @param pm     The {@link PersistenceManager} to use
+     * @param lastId ID of the last {@link Component} in the previous result set, or {@code null} if this is the first invocation
+     * @return A {@link List} representing a page of up to {@code 500} {@link Component}s
+     * @throws Exception When closing the query failed
+     * @see <a href="https://use-the-index-luke.com/no-offset">Keyset pagination</a>
+     */
+    private List<Component> fetchNextComponentsPage(final PersistenceManager pm, final Long lastId) throws Exception {
+        try (final Query<Component> query = pm.newQuery(Component.class)) {
+            if (lastId != null) {
+                query.setFilter("id < :lastId");
+                query.setParameters(lastId);
+            }
+            query.setOrdering("id DESC");
+            query.setRange(0, 500);
+            query.getFetchPlan().setGroup(Component.FetchGroup.INTERNAL_IDENTIFICATION.name());
+            return List.copyOf(query.executeList());
         }
     }
 

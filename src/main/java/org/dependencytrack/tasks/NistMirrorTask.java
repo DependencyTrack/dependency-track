af4edf64fb6ed4d070fab1353202765331be9bb1
Alexander Zenker <Alexander.Zenker@conplement.de>
2022-09-19 17:38:23 +0200

    Add some traces

--- src/main/java/org/dependencytrack/tasks/NistMirrorTask.java
+++ src/main/java/org/dependencytrack/tasks/NistMirrorTask.java
@@ -229,9 +229,9 @@ public class NistMirrorTask implements LoggableSubscriber {
                     LOGGER.info("Downloading...");
                     try (InputStream in = response.getEntity().getContent()) {
                         File temp = File.createTempFile(filename, null);
-                        file = new File(outputDir, filename);
+                        //file = new File(outputDir, filename);
                         FileUtils.copyInputStreamToFile(in, temp);
-                        LOGGER.info("Move...");
+                        LOGGER.info("Move " + temp.toPath() + " to " +  file.toPath());
                         Files.move(temp.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
                         if (ResourceType.CVE_YEAR_DATA == resourceType || ResourceType.CVE_MODIFIED_DATA == resourceType) {
                             // Sets the last modified date to 0. Upon a successful parse, it will be set back to its original date.

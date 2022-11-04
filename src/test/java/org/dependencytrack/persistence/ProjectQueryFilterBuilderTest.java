391638e8713948c74b36c9475727926f67902b1d
Pinto Spindler, Stephan <25225092+s-spindler@users.noreply.github.com>
2022-11-04 21:09:08 +0100

    Add tests for ProjectQueryFilterBuilder
    Signed-off-by: Pinto Spindler, Stephan <25225092+s-spindler@users.noreply.github.com>

---
+++ src/test/java/org/dependencytrack/persistence/ProjectQueryFilterBuilderTest.java
@@ -0,0 +1,35 @@
+package org.dependencytrack.persistence;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class ProjectQueryFilterBuilderTest {
+
+    @Test
+    public void testEmptyBuilderBuildsEmptyFilter() {
+        var builder = new ProjectQueryFilterBuilder();
+        var filter = builder.buildFilter();
+        assertNotNull(filter);
+        assertTrue(filter.isEmpty());
+    }
+
+    @Test
+    public void testEmptyBuilderBuildsEmptyParams() {
+        var builder = new ProjectQueryFilterBuilder();
+        var params = builder.getParams();
+        assertNotNull(params);
+        assertTrue(params.isEmpty());
+    }
+
+    @Test
+    public void testBuilderBuildsFilterAndParams() {
+        var testName = "test";
+        var builder = new ProjectQueryFilterBuilder().withName(testName);
+        assertEquals(Map.of("name", testName), builder.getParams());
+        assertEquals("(name == :name)", builder.buildFilter());
+    }
+
+}

0bcdcd52b6849fa5e36dc4c9de9e59b582d648e6
RBickert <rbt@mm-software.com>
2022-11-09 13:16:49 +0100

    Implement adding or deleting custom licenses
    Custom licenses can be added or deleted via the API.
    
    Adds new persistent attribute which tracks if a license is a custom
    license.
    
    Only custom licenses can be deleted.
    
    Adds tests for new functionalities.
    
    Signed-off-by: RBickert <rbt@mm-software.com>

--- src/main/java/org/dependencytrack/persistence/QueryManager.java
+++ src/main/java/org/dependencytrack/persistence/QueryManager.java
@@ -510,6 +510,18 @@ public class QueryManager extends AlpineQueryManager {
         return getLicenseQueryManager().synchronizeLicense(license, commitIndex);
     }
 
+    public License createLicense(String licenseId, String name, String text, String header, String template, boolean isOsiApproved, boolean isFsfLibre, boolean isDeprecatedLicenseId, boolean isCustomLicense, String comment, String seeAlso, boolean commitIndex) {
+        return getLicenseQueryManager().createLicense(licenseId, name, text, header, template, isOsiApproved, isFsfLibre, isDeprecatedLicenseId, isCustomLicense, comment, seeAlso, commitIndex);
+    }
+
+    public License createCustomLicense(License license, boolean commitIndex) {
+        return getLicenseQueryManager().createCustomLicense(license, commitIndex);
+    }
+
+    public void deleteLicense(final License license, final boolean commitIndex) {
+        getLicenseQueryManager().deleteLicense(license, commitIndex);
+    }
+
     public PaginatedResult getPolicies() {
         return getPolicyQueryManager().getPolicies();
     }

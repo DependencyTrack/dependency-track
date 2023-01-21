5908e934abee407c0a0549209ee8b732e2394a6b
Hendrik Borchardt <hendrik.borchardt@gmail.com>
2023-01-21 16:53:57 +0100

    Reimplement LicenseGroup policy evaluator to work with SpdxExpressions
    Recursively check license group compatibility with SpdxExpression,
    according to the SpdxExpressionOperations and whether the PolicyCondition
    specifies a positive list or negative list of licenses.
    
    As a special case, a `null` license group is interpreted as "unresolved"
    licenses, to be used by the LicensePolicyEvaluator.
    
    Signed-off-by: Hendrik Borchardt <hendrik.borchardt@gmail.com>

--- src/test/java/org/dependencytrack/policy/LicenseGroupPolicyEvaluatorTest.java
+++ src/test/java/org/dependencytrack/policy/LicenseGroupPolicyEvaluatorTest.java
@@ -27,11 +27,17 @@ import org.dependencytrack.model.PolicyCondition;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+@RunWith(JUnitParamsRunner.class)
 public class LicenseGroupPolicyEvaluatorTest extends PersistenceCapableTest {
 
     private PolicyEvaluator evaluator;
@@ -62,6 +68,104 @@ public class LicenseGroupPolicyEvaluatorTest extends PersistenceCapableTest {
         component.setResolvedLicense(license);
         List<PolicyConditionViolation> violations = evaluator.evaluate(policy, component);
         Assert.assertEquals(1, violations.size());
+    }
+
+    @Test
+    @Parameters(method = "forbiddenListTestcases")
+    public void spdxExpressionForbiddenList(String expression, Integer expectedViolations) {
+        {
+            License license = new License();
+            license.setName("MIT License");
+            license.setLicenseId("MIT");
+            license.setUuid(UUID.randomUUID());
+            license = qm.persist(license);
+        }
+        License license = new License();
+        license.setName("Apache 2.0");
+        license.setLicenseId("Apache-2.0");
+        license.setUuid(UUID.randomUUID());
+        license = qm.persist(license);
+        LicenseGroup lg = qm.createLicenseGroup("Test License Group");
+        lg.setLicenses(Collections.singletonList(license));
+        lg = qm.persist(lg);
+        lg = qm.detach(LicenseGroup.class, lg.getId());
+        license = qm.detach(License.class, license.getId());
+        Policy policy = qm.createPolicy("Test Policy", Policy.Operator.ANY, Policy.ViolationState.INFO);
+
+        // Operator.IS means it is a forbid list
+        PolicyCondition condition = qm.createPolicyCondition(policy, PolicyCondition.Subject.LICENSE_GROUP,
+                PolicyCondition.Operator.IS, lg.getUuid().toString());
+        policy = qm.detach(Policy.class, policy.getId());
+        qm.detach(PolicyCondition.class, condition.getId());
+
+        Component component = new Component();
+        component.setLicenseExpression(expression);
+        List<PolicyConditionViolation> violations = evaluator.evaluate(policy, component);
+        Assert.assertEquals(expectedViolations.intValue(), violations.size());
+    }
+    
+    private Object[] forbiddenListTestcases() {
+        return new Object[] {
+            // nonexistent license means it is not on the negative list
+            new Object[] { "Apache-2.0 OR NonexistentLicense", 0 },
+            // Apache is on the negative list, violation
+            new Object[] { "Apache-2.0 AND(MIT OR NonexistentLicense OR Apache-2.0)AND(Apache-2.0 AND Apache-2.0)", 1},
+            // Apache is on the negative list, violation
+            new Object[] { "Apache-2.0 AND NonexistentLicense", 1},
+            // MIT allowed
+            new Object[] { "Apache-2.0 OR MIT", 0 }
+        };
+    }
+
+    @Test
+    @Parameters(method = "allowListTestcases")
+    public void spdxExpressionAllowList(String licenseName, Integer expectedViolations) {
+        {
+            License license = new License();
+            license.setName("MIT License");
+            license.setLicenseId("MIT");
+            license.setUuid(UUID.randomUUID());
+            license = qm.persist(license);
+        }
+        License license = new License();
+        license.setName("Apache 2.0");
+        license.setLicenseId("Apache-2.0");
+        license.setUuid(UUID.randomUUID());
+        license = qm.persist(license);
+        LicenseGroup lg = qm.createLicenseGroup("Test License Group");
+        lg.setLicenses(Collections.singletonList(license));
+        lg = qm.persist(lg);
+        lg = qm.detach(LicenseGroup.class, lg.getId());
+        license = qm.detach(License.class, license.getId());
+        Policy policy = qm.createPolicy("Test Policy", Policy.Operator.ANY, Policy.ViolationState.INFO);
+
+        // Operator.IS_NOT means it is a positive list
+        PolicyCondition condition = qm.createPolicyCondition(policy, PolicyCondition.Subject.LICENSE_GROUP,
+                PolicyCondition.Operator.IS_NOT, lg.getUuid().toString());
+        policy = qm.detach(Policy.class, policy.getId());
+        qm.detach(PolicyCondition.class, condition.getId());
+
+        Policy _policy = policy;
+        
+        Component component = new Component();
+        component.setLicenseExpression(licenseName);
+        List<PolicyConditionViolation> violations = evaluator.evaluate(_policy, component);
+        Assert.assertEquals("Error for: " + licenseName, expectedViolations.intValue(), violations.size());
+    }
+    
+    private Object[] allowListTestcases() {
+        return new Object[] {
+            // Nonexistent license is not in positive list, violation
+            //new Object[] { "NonexistentLicense", 1},
+            // Apache is on the positive list
+            //new Object[] { "Apache-2.0 OR NonexistentLicense", 0},
+            // Apache is on the positive list
+            new Object[] { "Apache-2.0 AND(MIT OR NonexistentLicense OR Apache-2.0)AND(Apache-2.0 AND Apache-2.0)", 0},
+            // Nonexistent is not on the positive list, violation
+            new Object[] { "Apache-2.0 AND NonexistentLicense", 1},
+            // Apache allowed
+            new Object[] { "Apache-2.0 OR MIT", 0}
+        };
     }
 
     @Test

51795b20abe76fe5d5960c0d767f0a8288e0acd3
jason <jason.irwin@idoxgroup.com>
2023-01-31 15:36:23 +0000

    Fixed broken imports
    Signed-off-by: jason <jason.irwin@idoxgroup.com>

--- src/main/java/org/dependencytrack/parser/cyclonedx/util/ModelConverter.java
+++ src/main/java/org/dependencytrack/parser/cyclonedx/util/ModelConverter.java
@@ -58,7 +58,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ModelConverter {
 

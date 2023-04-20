1897537bbe039f3796d8bb1f6111970643fd4692
Florian Heubeck <heubeck@mediamarktsaturn.com>
2023-04-20 12:51:12 +0200

    Issue #2695: Add externalReferences to PATCH project
    Signed-off-by: Florian Heubeck <heubeck@mediamarktsaturn.com>

--- src/test/java/org/dependencytrack/resources/v1/ProjectResourceTest.java
+++ src/test/java/org/dependencytrack/resources/v1/ProjectResourceTest.java
@@ -22,8 +22,10 @@ import alpine.common.util.UuidUtil;
 import alpine.notification.Notification;
 import alpine.server.filters.ApiFilter;
 import alpine.server.filters.AuthenticationFilter;
+import org.cyclonedx.model.ExternalReference.Type;
 import org.dependencytrack.ResourceTest;
 import org.dependencytrack.model.ConfigPropertyConstants;
+import org.dependencytrack.model.ExternalReference;
 import org.dependencytrack.model.Project;
 import org.dependencytrack.model.Tag;
 import org.glassfish.jersey.client.HttpUrlConnectorProvider;
@@ -542,6 +544,39 @@ public class ProjectResourceTest extends ResourceTest {
         final var jsonTags = json.getJsonArray("tags");
         Assert.assertEquals(1, jsonTags.size());
         Assert.assertEquals("tag4", jsonTags.get(0).asJsonObject().getString("name"));
+    }
+
+    @Test
+    public void patchProjectExternalReferencesTest() {
+        final var project = qm.createProject("referred-project", "ExtRef test project", "1.0", null, null, null, true, false);
+        final var ref1 = new ExternalReference();
+        ref1.setType(Type.VCS);
+        ref1.setUrl("https://github.com/DependencyTrack/awesomeness");
+        final var ref2 = new ExternalReference();
+        ref2.setType(Type.WEBSITE);
+        ref2.setUrl("https://dependencytrack.org");
+        ref2.setComment("Worth a visit!");
+        final var externalReferences = List.of(ref1, ref2);
+        final var jsonProject = new Project();
+        jsonProject.setExternalReferences(externalReferences);
+
+        final var response = target(V1_PROJECT + "/" + project.getUuid())
+                .request()
+                .header(X_API_KEY, apiKey)
+                .property(HttpUrlConnectorProvider.SET_METHOD_WORKAROUND, true)
+                .method("PATCH", Entity.json(jsonProject));
+
+        Assert.assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        final var json = parseJsonObject(response);
+        final var patchedExternalReferences = json.getJsonArray("externalReferences");
+        Assert.assertEquals(2, patchedExternalReferences.size());
+        final var patchedRef1 = patchedExternalReferences.getJsonObject(0);
+        final var patchedRef2 = patchedExternalReferences.getJsonObject(1);
+        Assert.assertEquals("vcs", patchedRef1.getString("type"));
+        Assert.assertEquals("https://github.com/DependencyTrack/awesomeness", patchedRef1.getString("url"));
+        Assert.assertEquals("website", patchedRef2.getString("type"));
+        Assert.assertEquals("https://dependencytrack.org", patchedRef2.getString("url"));
+        Assert.assertEquals("Worth a visit!", patchedRef2.getString("comment"));
     }
 
     @Test

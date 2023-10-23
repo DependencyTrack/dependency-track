751218fbb140a384d6682b28ee5fff0a9cf78dd2
nscuro <nscuro@protonmail.com>
2023-10-23 19:27:59 +0200

    Fix test failures due to already closed `PersistenceManager`
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/test/java/org/dependencytrack/PersistenceCapableTest.java
+++ src/test/java/org/dependencytrack/PersistenceCapableTest.java
@@ -45,7 +45,8 @@ public abstract class PersistenceCapableTest {
         // that was neither committed nor rolled back. Unfortunately some areas of the
         // code base can leave such a broken state behind if they run into unexpected
         // errors. See: https://github.com/DependencyTrack/dependency-track/issues/2677
-        if (qm.getPersistenceManager().currentTransaction().isActive()) {
+        if (!qm.getPersistenceManager().isClosed()
+                && qm.getPersistenceManager().currentTransaction().isActive()) {
             qm.getPersistenceManager().currentTransaction().rollback();
         }
 

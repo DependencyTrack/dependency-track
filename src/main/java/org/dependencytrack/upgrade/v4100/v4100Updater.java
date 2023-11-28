561d7cff83c95e97474c61c3c8a021e8ade7252a
nscuro <nscuro@protonmail.com>
2023-11-28 23:14:09 +0100

    Add migration to populate new `AUTHENTICATIONREQUIRED` column
    Only affects internal projects with credentials. This is a follow-up to #2870, which originally added the new column in order to support authentication for non-internal repositories.
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/main/java/org/dependencytrack/upgrade/v4100/v4100Updater.java
+++ src/main/java/org/dependencytrack/upgrade/v4100/v4100Updater.java
@@ -23,6 +23,7 @@ import alpine.persistence.AlpineQueryManager;
 import alpine.server.upgrade.AbstractUpgradeItem;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class v4100Updater extends AbstractUpgradeItem {
@@ -37,12 +38,31 @@ public class v4100Updater extends AbstractUpgradeItem {
     @Override
     public void executeUpgrade(final AlpineQueryManager qm, final Connection connection) throws Exception {
         dropCpeTable(connection);
+        authRequiredForInternalReposWithCredentials(connection);
     }
 
     private static void dropCpeTable(final Connection connection) throws Exception {
         LOGGER.info("Dropping CPE table");
         try (final Statement stmt = connection.createStatement()) {
             stmt.execute("DROP TABLE \"CPE\"");
+        }
+    }
+
+    private static void authRequiredForInternalReposWithCredentials(final Connection connection) throws Exception {
+        LOGGER.info("Marking internal repositories with credentials as \"requires authentication\"");
+        try (final PreparedStatement ps = connection.prepareStatement("""
+                UPDATE "REPOSITORY"
+                SET "AUTHENTICATIONREQUIRED" = ?
+                WHERE "INTERNAL" = ? AND ("USERNAME" IS NOT NULL OR "PASSWORD" IS NOT NULL)
+                """)) {
+            ps.setBoolean(1, true);
+            ps.setBoolean(2, true);
+            final long updatedRepositories = ps.executeUpdate();
+            if (updatedRepositories == 0) {
+                LOGGER.info("No repositories had to be updated");
+            } else {
+                LOGGER.info("Updated %d repositories".formatted(updatedRepositories));
+            }
         }
     }
 

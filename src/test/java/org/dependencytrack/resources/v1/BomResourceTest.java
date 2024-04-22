0f3cbe2ae028ef755c5eb09f581ddcf7d76962ff
Marlon Pina Tojal <marlont@backbase.com>
2024-04-22 11:49:36 +0200

    fix test
    Signed-off-by: Marlon Pina Tojal <marlont@backbase.com>

--- src/test/java/org/dependencytrack/resources/v1/BomResourceTest.java
+++ src/test/java/org/dependencytrack/resources/v1/BomResourceTest.java
@@ -64,6 +64,7 @@ import static org.apache.commons.io.IOUtils.resourceToByteArray;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.dependencytrack.model.ConfigPropertyConstants.BOM_VALIDATION_ENABLED;
 
 public class BomResourceTest extends ResourceTest {
 
@@ -870,6 +871,14 @@ public class BomResourceTest extends ResourceTest {
     @Test
     public void uploadBomInvalidJsonTest() {
         initializeWithPermissions(Permissions.BOM_UPLOAD);
+
+        qm.createConfigProperty(
+                BOM_VALIDATION_ENABLED.getGroupName(),
+                BOM_VALIDATION_ENABLED.getPropertyName(),
+                "true",
+                BOM_VALIDATION_ENABLED.getPropertyType(),
+                null
+        );
 
         final var project = new Project();
         project.setName("acme-app");

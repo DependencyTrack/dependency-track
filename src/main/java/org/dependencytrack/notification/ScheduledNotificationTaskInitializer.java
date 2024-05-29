d9fe8d118dedceb4635ebe0bf891458a82ef135c
Max Schiller <msr@mm-software.com>
2024-05-29 10:31:43 +0200

    initialize scheduled notification tasks at startup
    Signed-off-by: Max Schiller <msr@mm-software.com>

---
+++ src/main/java/org/dependencytrack/notification/ScheduledNotificationTaskInitializer.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of Dependency-Track.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) OWASP Foundation. All Rights Reserved.
+ */
+package org.dependencytrack.notification;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.dependencytrack.model.ScheduledNotificationRule;
+import org.dependencytrack.persistence.QueryManager;
+
+import com.asahaf.javacron.InvalidExpressionException;
+import com.asahaf.javacron.Schedule;
+
+import alpine.common.logging.Logger;
+
+public class ScheduledNotificationTaskInitializer implements ServletContextListener {
+    private static final Logger LOGGER = Logger.getLogger(ScheduledNotificationTaskInitializer.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try (var qm = new QueryManager()) {
+            var paginatedScheduledRules = qm.getScheduledNotificationRules();
+            var scheduledRulesList = paginatedScheduledRules.getList(ScheduledNotificationRule.class);
+            for (var scheduledRule : scheduledRulesList) {
+                try {
+                    if (scheduledRule.isEnabled()) {
+                        ScheduledNotificationTaskManager.scheduleNextRuleTask(
+                                scheduledRule.getUuid(),
+                                Schedule.create(scheduledRule.getCronConfig()));
+                    }
+                } catch (InvalidExpressionException e) {
+                    LOGGER.error("Invalid cron expression in rule " + scheduledRule.getUuid() + ", no cron task could be created!");
+                }
+            }
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        ScheduledNotificationTaskManager.cancelAllActiveRuleTasks();
+    }
+}

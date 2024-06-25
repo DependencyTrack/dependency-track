24e1773b01c5d11dfa5d72c0f3861df14bbb31f6
nscuro <nscuro@protonmail.com>
2024-06-25 23:02:53 +0200

    Add REST endpoints for tag retrieval
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/main/java/org/dependencytrack/persistence/TagQueryManager.java
+++ src/main/java/org/dependencytrack/persistence/TagQueryManager.java
@@ -19,6 +19,7 @@
 package org.dependencytrack.persistence;
 
 import alpine.common.logging.Logger;
+import alpine.persistence.OrderDirection;
 import alpine.persistence.PaginatedResult;
 import alpine.resources.AlpineRequest;
 import org.dependencytrack.model.Policy;
@@ -26,8 +27,12 @@ import org.dependencytrack.model.Project;
 import org.dependencytrack.model.Tag;
 
 import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class TagQueryManager extends QueryManager implements IQueryManager {
@@ -54,6 +59,198 @@ public class TagQueryManager extends QueryManager implements IQueryManager {
         super(pm, request);
     }
 
+    /**
+     * @since 4.12.0
+     */
+    public record TagListRow(String name, long projectCount, long policyCount, long totalCount) {
+
+        @SuppressWarnings("unused") // DataNucleus will use this for MSSQL.
+        public TagListRow(String name, int projectCount, int policyCount, int totalCount) {
+            this(name, (long) projectCount, (long) policyCount, (long) totalCount);
+        }
+
+    }
+
+    /**
+     * @since 4.12.0
+     */
+    @Override
+    public List<TagListRow> getTags() {
+        final Map.Entry<String, Map<String, Object>> projectAclConditionAndParams = getProjectAclSqlCondition();
+        final String projectAclCondition = projectAclConditionAndParams.getKey();
+        final Map<String, Object> projectAclConditionParams = projectAclConditionAndParams.getValue();
+
+        // language=SQL
+        var sqlQuery = """
+                SELECT "NAME" AS "name"
+                     , (SELECT COUNT(*)
+                          FROM "PROJECTS_TAGS"
+                         INNER JOIN "PROJECT"
+                            ON "PROJECT"."ID" = "PROJECTS_TAGS"."PROJECT_ID"
+                         WHERE "PROJECTS_TAGS"."TAG_ID" = "TAG"."ID"
+                           AND %s
+                       ) AS "projectCount"
+                     , (SELECT COUNT(*)
+                          FROM "POLICY_TAGS"
+                         WHERE "POLICY_TAGS"."TAG_ID" = "TAG"."ID"
+                       ) AS "policyCount"
+                     , COUNT(*) OVER() AS "totalCount"
+                  FROM "TAG"
+                """.formatted(projectAclCondition);
+
+        final var params = new HashMap<>(projectAclConditionParams);
+
+        if (filter != null) {
+            sqlQuery += " WHERE \"NAME\" LIKE :nameFilter";
+            params.put("nameFilter", "%" + filter.toLowerCase() + "%");
+        }
+
+        if (orderBy == null) {
+            sqlQuery += " ORDER BY \"name\" ASC";
+        } else if ("name".equals(orderBy) || "projectCount".equals(orderBy) || "policyCount".equals(orderBy)) {
+            sqlQuery += " ORDER BY \"%s\" %s, \"ID\" ASC".formatted(orderBy,
+                    orderDirection == OrderDirection.DESCENDING ? "DESC" : "ASC");
+        } else {
+            // TODO: Throw NotSortableException once Alpine opens up its constructor.
+            throw new IllegalArgumentException("Cannot sort by " + orderBy);
+        }
+
+        sqlQuery += " " + getOffsetLimitSqlClause();
+
+        final Query<?> query = pm.newQuery(Query.SQL, sqlQuery);
+        query.setNamedParameters(params);
+        try {
+            return new ArrayList<>(query.executeResultList(TagListRow.class));
+        } finally {
+            query.closeAll();
+        }
+    }
+
+    /**
+     * @since 4.12.0
+     */
+    public record TaggedProjectRow(String uuid, String name, String version, long totalCount) {
+
+        @SuppressWarnings("unused") // DataNucleus will use this for MSSQL.
+        public TaggedProjectRow(String uuid, String name, String version, int totalCount) {
+            this(uuid, name, version, (long) totalCount);
+        }
+
+    }
+
+    /**
+     * @since 4.12.0
+     */
+    @Override
+    public List<TaggedProjectRow> getTaggedProjects(final String tagName) {
+        final Map.Entry<String, Map<String, Object>> projectAclConditionAndParams = getProjectAclSqlCondition();
+        final String projectAclCondition = projectAclConditionAndParams.getKey();
+        final Map<String, Object> projectAclConditionParams = projectAclConditionAndParams.getValue();
+
+        // language=SQL
+        var sqlQuery = """
+            SELECT "PROJECT"."UUID" AS "uuid"
+                 , "PROJECT"."NAME" AS "name"
+                 , "PROJECT"."VERSION" AS "version"
+                 , COUNT(*) OVER() AS "totalCount"
+              FROM "PROJECT"
+             INNER JOIN "PROJECTS_TAGS"
+                ON "PROJECTS_TAGS"."PROJECT_ID" = "PROJECT"."ID"
+             INNER JOIN "TAG"
+                ON "TAG"."ID" = "PROJECTS_TAGS"."TAG_ID"
+             WHERE "TAG"."NAME" = :tag
+               AND %s
+            """.formatted(projectAclCondition);
+
+        final var params = new HashMap<>(projectAclConditionParams);
+        params.put("tag", tagName);
+
+        if (filter != null) {
+            sqlQuery += " AND \"PROJECT\".\"NAME\" LIKE :nameFilter";
+            params.put("nameFilter", "%" + filter + "%");
+        }
+
+        if (orderBy == null) {
+            sqlQuery += " ORDER BY \"name\" ASC, \"version\" DESC";
+        } else if ("name".equals(orderBy) || "version".equals(orderBy)) {
+            sqlQuery += " ORDER BY \"%s\" %s, \"ID\" ASC".formatted(orderBy,
+                    orderDirection == OrderDirection.DESCENDING ? "DESC" : "ASC");
+        } else {
+            // TODO: Throw NotSortableException once Alpine opens up its constructor.
+            throw new IllegalArgumentException("Cannot sort by " + orderBy);
+        }
+
+        sqlQuery += " " + getOffsetLimitSqlClause();
+
+        final Query<?> query = pm.newQuery(Query.SQL, sqlQuery);
+        query.setNamedParameters(params);
+        try {
+            return new ArrayList<>(query.executeResultList(TaggedProjectRow.class));
+        } finally {
+            query.closeAll();
+        }
+    }
+
+    /**
+     * @since 4.12.0
+     */
+    public record TaggedPolicyRow(String uuid, String name, long totalCount) {
+
+        @SuppressWarnings("unused") // DataNucleus will use this for MSSQL.
+        public TaggedPolicyRow(String uuid, String name, int totalCount) {
+            this(uuid, name, (long) totalCount);
+        }
+
+    }
+
+    /**
+     * @since 4.12.0
+     */
+    @Override
+    public List<TaggedPolicyRow> getTaggedPolicies(final String tagName) {
+        // language=SQL
+        var sqlQuery = """
+                SELECT "POLICY"."UUID" AS "uuid"
+                     , "POLICY"."NAME" AS "name"
+                     , COUNT(*) OVER() AS "totalCount"
+                  FROM "POLICY"
+                 INNER JOIN "POLICY_TAGS"
+                    ON "POLICY_TAGS"."POLICY_ID" = "POLICY"."ID"
+                 INNER JOIN "TAG"
+                    ON "TAG"."ID" = "POLICY_TAGS"."TAG_ID"
+                 WHERE "TAG"."NAME" = :tag
+                """;
+
+        final var params = new HashMap<String, Object>();
+        params.put("tag", tagName);
+
+        if (filter != null) {
+            sqlQuery += " AND \"POLICY\".\"NAME\" LIKE :nameFilter";
+            params.put("nameFilter", "%" + filter + "%");
+        }
+
+        if (orderBy == null) {
+            sqlQuery += " ORDER BY \"name\" ASC";
+        } else if ("name".equals(orderBy)) {
+            sqlQuery += " ORDER BY \"%s\" %s".formatted(orderBy,
+                    orderDirection == OrderDirection.DESCENDING ? "DESC" : "ASC");
+        } else {
+            // TODO: Throw NotSortableException once Alpine opens up its constructor.
+            throw new IllegalArgumentException("Cannot sort by " + orderBy);
+        }
+
+        sqlQuery += " " + getOffsetLimitSqlClause();
+
+        final Query<?> query = pm.newQuery(Query.SQL, sqlQuery);
+        query.setNamedParameters(params);
+        try {
+            return new ArrayList<>(query.executeResultList(TaggedPolicyRow.class));
+        } finally {
+            query.closeAll();
+        }
+    }
+
+    @Override
     public PaginatedResult getTags(String policyUuid) {
 
         LOGGER.debug("Retrieving tags under policy " + policyUuid);

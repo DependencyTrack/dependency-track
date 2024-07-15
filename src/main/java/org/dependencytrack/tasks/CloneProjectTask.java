164847a87ad747c3128793c7b0fb01d4b06067cf
nscuro <nscuro@protonmail.com>
2024-07-15 20:54:27 +0200

    Ensure no unique constraint violation for `ProjectMetadata`
    Adds regression test for #3895. The behavior itself does not reproduce on `master`, but does with `4.11.5`.
    
    Wraps the project cloning in a transaction to make it more reliable. Adds MDC variables for `CloneProjectTask` (#3234).
    
    Fixes #3895
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/main/java/org/dependencytrack/tasks/CloneProjectTask.java
+++ src/main/java/org/dependencytrack/tasks/CloneProjectTask.java
@@ -25,8 +25,12 @@ import org.dependencytrack.event.CloneProjectEvent;
 import org.dependencytrack.model.Project;
 import org.dependencytrack.persistence.QueryManager;
 import org.dependencytrack.resources.v1.vo.CloneProjectRequest;
+import org.slf4j.MDC;
 
 import java.util.UUID;
+
+import static org.dependencytrack.common.MdcKeys.MDC_EVENT_TOKEN;
+import static org.dependencytrack.common.MdcKeys.MDC_PROJECT_UUID;
 
 public class CloneProjectTask implements Subscriber {
 
@@ -36,15 +40,26 @@ public class CloneProjectTask implements Subscriber {
      * {@inheritDoc}
      */
     public void inform(final Event e) {
-        if (e instanceof CloneProjectEvent) {
-            final CloneProjectEvent event = (CloneProjectEvent)e;
+        if (e instanceof final CloneProjectEvent event) {
             final CloneProjectRequest request = event.getRequest();
-            LOGGER.info("Cloning project: " + request.getProject());
-            try (QueryManager qm = new QueryManager()) {
-                final Project project = qm.clone(UUID.fromString(request.getProject()),
-                        request.getVersion(), request.includeTags(), request.includeProperties(),
-                        request.includeComponents(), request.includeServices(), request.includeAuditHistory(), request.includeACL(), request.includePolicyViolations());
-                LOGGER.info("Cloned project: " + request.getProject() + " to " + project.getUuid());
+            try (var ignoredMdcProjectUuid = MDC.putCloseable(MDC_PROJECT_UUID, request.getProject());
+                 var ignoredMdcEventToken = MDC.putCloseable(MDC_EVENT_TOKEN, event.getChainIdentifier().toString());
+                 final var qm = new QueryManager()) {
+                LOGGER.info("Cloning project for version %s".formatted(request.getVersion()));
+                final Project project = qm.clone(
+                        UUID.fromString(request.getProject()),
+                        request.getVersion(),
+                        request.includeTags(),
+                        request.includeProperties(),
+                        request.includeComponents(),
+                        request.includeServices(),
+                        request.includeAuditHistory(),
+                        request.includeACL(),
+                        request.includePolicyViolations()
+                );
+                LOGGER.info("Cloned project for version %s into project %s".formatted(project.getVersion(), project.getUuid()));
+            } catch (RuntimeException ex) {
+                LOGGER.error("Failed to clone project", ex);
             }
         }
     }

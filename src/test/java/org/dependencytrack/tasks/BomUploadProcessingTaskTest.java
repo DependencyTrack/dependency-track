79ba79fc3cb2d0c8e39ce06e6fda214a95376e1a
Ross Murphy <RossMurphy@ibm.com>
2024-08-12 20:44:00 +0100

    remove unused import
    Signed-off-by: Ross Murphy <RossMurphy@ibm.com>

--- src/test/java/org/dependencytrack/tasks/BomUploadProcessingTaskTest.java
+++ src/test/java/org/dependencytrack/tasks/BomUploadProcessingTaskTest.java
@@ -71,7 +71,6 @@ import static org.assertj.core.api.Assertions.fail;
 import static org.awaitility.Awaitility.await;
 import static org.dependencytrack.assertion.Assertions.assertConditionWithTimeout;
 import static org.dependencytrack.model.ConfigPropertyConstants.BOM_VALIDATION_ENABLED;
-import static org.junit.Assert.assertEquals;
 
 public class BomUploadProcessingTaskTest extends PersistenceCapableTest {
 

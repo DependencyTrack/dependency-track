fc4498a9ec9fa819da2939c40dcf7358607519eb
nscuro <nscuro@protonmail.com>
2024-08-14 12:59:11 +0200

    Fix `directDependencies` missing from `/api/v1/project/{uuid}` response when not already cached
    Since #4049, the endpoint returns only properties that are part of the `ALL` fetch group. `Project#directDependencies` was missing from this fetch group, and thus was not returned by the API, **unless the project was previously created or updated**.
    
    The API ended up returning the `directDependencies` field after modification of the project, which happened because `directDependencies` was in the L2 cache at that point.
    
    Besides fixing the issue at hand, this change also extends the existing endpoint test to ensure that **all** fields are returned as expected.
    
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/main/java/org/dependencytrack/model/ProjectMetadata.java
+++ src/main/java/org/dependencytrack/model/ProjectMetadata.java
@@ -26,6 +26,8 @@ import org.dependencytrack.persistence.converter.OrganizationalEntityJsonConvert
 
 import javax.jdo.annotations.Column;
 import javax.jdo.annotations.Convert;
+import javax.jdo.annotations.FetchGroup;
+import javax.jdo.annotations.FetchGroups;
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
@@ -41,6 +43,12 @@ import java.util.List;
  *
  * @since 4.10.0
  */
+@FetchGroups(value = {
+        @FetchGroup(name = "ALL", members = {
+                @Persistent(name = "supplier"),
+                @Persistent(name = "authors")
+        })
+})
 @PersistenceCapable(table = "PROJECT_METADATA")
 @JsonInclude(Include.NON_NULL)
 public class ProjectMetadata {

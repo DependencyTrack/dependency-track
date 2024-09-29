4665b5316a8ee38d5f2c5930f80c16904a1f8460
Kirill.Sybin <kirill.sybin@lex-com.net>
2024-09-29 15:34:20 +0200

    Resurrect enable badges setting for deprecation
    To make the removal of unauthenticated access to badges not be a
    breaking change after all, the enable badges config property is kept in
    after all, but repurposed into a setting to enable unauthenticated
    access to the badges resource. If it is disabled, then the badges api
    remains accessible to authenticated and authorized requests.
    
    Signed-off-by: Kirill.Sybin <kirill.sybin@lex-com.net>

--- src/main/java/org/dependencytrack/resources/v1/BadgeResource.java
+++ src/main/java/org/dependencytrack/resources/v1/BadgeResource.java
@@ -18,8 +18,18 @@
  */
 package org.dependencytrack.resources.v1;
 
-import alpine.server.auth.AllowApiKeyInQueryParameter;
-import alpine.server.auth.PermissionRequired;
+import alpine.common.logging.Logger;
+import alpine.common.util.BooleanUtil;
+import alpine.model.ApiKey;
+import alpine.model.ConfigProperty;
+import alpine.model.UserPrincipal;
+import alpine.model.LdapUser;
+import alpine.model.ManagedUser;
+import alpine.model.OidcUser;
+import alpine.server.auth.ApiKeyAuthenticationService;
+import alpine.server.auth.JwtAuthenticationService;
+import alpine.server.auth.AuthenticationNotRequired;
+import alpine.server.filters.AuthenticationFilter;
 import alpine.server.resources.AlpineResource;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -40,8 +50,16 @@ import org.dependencytrack.resources.v1.misc.Badger;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.HttpMethod;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
+import org.glassfish.jersey.server.ContainerRequest;
+import org.owasp.security.logging.SecurityMarkers;
+
+import javax.naming.AuthenticationException;
+import java.security.Principal;
+
+import static org.dependencytrack.model.ConfigPropertyConstants.GENERAL_BADGE_ENABLED;
 
 /**
  * JAX-RS resources for processing metrics.
@@ -60,6 +78,97 @@ public class BadgeResource extends AlpineResource {
 
     private static final String SVG_MEDIA_TYPE = "image/svg+xml";
 
+    private final Logger LOGGER = Logger.getLogger(AuthenticationFilter.class);
+
+    private boolean isUnauthenticatedBadgeAccessEnabled(final QueryManager qm) {
+        ConfigProperty property = qm.getConfigProperty(
+                GENERAL_BADGE_ENABLED.getGroupName(), GENERAL_BADGE_ENABLED.getPropertyName());
+        return BooleanUtil.valueOf(property.getPropertyValue());
+    }
+
+    // Stand-in methods for alpine.server.filters.AuthenticationFilter and
+    // alpine.server.filters.AuthorizationFilter to allow enabling and disabling of
+    // unauthenticated access to the badges API during runtime, used solely to offer
+    // a deprecation period for unauthenticated access to badges.
+    private boolean passesAuthentication() {
+        ContainerRequest request = (ContainerRequest) super.getRequestContext().getRequest();
+
+        if (HttpMethod.OPTIONS.equals(request.getMethod())) {
+            return true;
+        }
+
+        Principal principal = null;
+
+        final ApiKeyAuthenticationService apiKeyAuthService = new ApiKeyAuthenticationService(request, true);
+        if (apiKeyAuthService.isSpecified()) {
+            try {
+                principal = apiKeyAuthService.authenticate();
+            } catch (AuthenticationException e) {
+                LOGGER.info(SecurityMarkers.SECURITY_FAILURE, "Invalid API key asserted");
+                return false;
+            }
+        }
+
+        final JwtAuthenticationService jwtAuthService = new JwtAuthenticationService(request);
+        if (jwtAuthService.isSpecified()) {
+            try {
+                principal = jwtAuthService.authenticate();
+            } catch (AuthenticationException e) {
+                LOGGER.info(SecurityMarkers.SECURITY_FAILURE, "Invalid JWT asserted");
+                return false;
+            }
+        }
+
+        if (principal == null) {
+            return false;
+        } else {
+            super.getRequestContext().setProperty("Principal", principal);
+            return true;
+        }
+    }
+
+    private boolean passesAuthorization(final QueryManager qm) {
+        final Principal principal = (Principal) super.getRequestContext().getProperty("Principal");
+        if (principal == null) {
+            LOGGER.info(SecurityMarkers.SECURITY_FAILURE, "A request was made without the assertion of a valid user principal");
+            return false;
+        }
+
+        final String[] permissions = { Permissions.Constants.VIEW_BADGES };
+
+        if (principal instanceof ApiKey) {
+            final ApiKey apiKey = (ApiKey)principal;
+            for (final String permission: permissions) {
+                if (qm.hasPermission(apiKey, permission)) {
+                    return true;
+                }
+            }
+            LOGGER.info(SecurityMarkers.SECURITY_FAILURE, "Unauthorized access attempt made by API Key "
+                    + apiKey.getMaskedKey() + " to " + ((ContainerRequest) super.getRequestContext()).getRequestUri().toString());
+        } else {
+            UserPrincipal user = null;
+            if (principal instanceof ManagedUser) {
+                user = qm.getManagedUser(((ManagedUser) principal).getUsername());
+            } else if (principal instanceof LdapUser) {
+                user = qm.getLdapUser(((LdapUser) principal).getUsername());
+            } else if (principal instanceof OidcUser) {
+                user = qm.getOidcUser(((OidcUser) principal).getUsername());
+            }
+            if (user == null) {
+                LOGGER.info(SecurityMarkers.SECURITY_FAILURE, "A request was made but the system in unable to find the user principal");
+                return false;
+            }
+            for (final String permission : permissions) {
+                if (qm.hasPermission(user, permission, true)) {
+                    return true;
+                }
+            }
+            LOGGER.info(SecurityMarkers.SECURITY_FAILURE, "Unauthorized access attempt made by "
+                    + user.getUsername() + " to " + ((ContainerRequest) super.getRequestContext()).getRequestUri().toString());
+        }
+        return false;
+    }
+
     @GET
     @Path("/vulns/project/{uuid}")
     @Produces(SVG_MEDIA_TYPE)
@@ -74,17 +183,23 @@ public class BadgeResource extends AlpineResource {
                     content = @Content(schema = @Schema(type = "string"))
             ),
             @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
             @ApiResponse(responseCode = "404", description = "The project could not be found")
     })
-    @PermissionRequired(Permissions.Constants.VIEW_BADGES)
-    @AllowApiKeyInQueryParameter
+    @AuthenticationNotRequired
     public Response getProjectVulnerabilitiesBadge(
             @Parameter(description = "The UUID of the project to retrieve metrics for", schema = @Schema(type = "string", format = "uuid"), required = true)
             @PathParam("uuid") @ValidUuid String uuid) {
         try (QueryManager qm = new QueryManager()) {
+            if (!isUnauthenticatedBadgeAccessEnabled(qm) && !passesAuthentication()) {
+                return Response.status(Response.Status.UNAUTHORIZED).build();
+            }
+            if (!isUnauthenticatedBadgeAccessEnabled(qm) && !passesAuthorization(qm)) {
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
             final Project project = qm.getObjectByUuid(Project.class, uuid);
             if (project != null) {
-                if (!qm.hasAccess(super.getPrincipal(), project)) {
+                if (!isUnauthenticatedBadgeAccessEnabled(qm) && !qm.hasAccess(super.getPrincipal(), project)) {
                     return Response.status(Response.Status.FORBIDDEN).entity("Access to the specified project is forbidden").build();
                 }
                 final ProjectMetrics metrics = qm.getMostRecentProjectMetrics(project);
@@ -110,19 +225,25 @@ public class BadgeResource extends AlpineResource {
                     content = @Content(schema = @Schema(type = "string"))
             ),
             @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
             @ApiResponse(responseCode = "404", description = "The project could not be found")
     })
-    @PermissionRequired(Permissions.Constants.VIEW_BADGES)
-    @AllowApiKeyInQueryParameter
+    @AuthenticationNotRequired
     public Response getProjectVulnerabilitiesBadge(
             @Parameter(description = "The name of the project to query on", required = true)
             @PathParam("name") String name,
             @Parameter(description = "The version of the project to query on", required = true)
             @PathParam("version") String version) {
         try (QueryManager qm = new QueryManager()) {
+            if (!isUnauthenticatedBadgeAccessEnabled(qm) && !passesAuthentication()) {
+                return Response.status(Response.Status.UNAUTHORIZED).build();
+            }
+            if (!isUnauthenticatedBadgeAccessEnabled(qm) && !passesAuthorization(qm)) {
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
             final Project project = qm.getProject(name, version);
             if (project != null) {
-                if (!qm.hasAccess(super.getPrincipal(), project)) {
+                if (!isUnauthenticatedBadgeAccessEnabled(qm) && !qm.hasAccess(super.getPrincipal(), project)) {
                     return Response.status(Response.Status.FORBIDDEN).entity("Access to the specified project is forbidden").build();
                 }
                 final ProjectMetrics metrics = qm.getMostRecentProjectMetrics(project);
@@ -148,17 +269,23 @@ public class BadgeResource extends AlpineResource {
                     content = @Content(schema = @Schema(type = "string"))
             ),
             @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
             @ApiResponse(responseCode = "404", description = "The project could not be found")
     })
-    @PermissionRequired(Permissions.Constants.VIEW_BADGES)
-    @AllowApiKeyInQueryParameter
+    @AuthenticationNotRequired
     public Response getProjectPolicyViolationsBadge(
             @Parameter(description = "The UUID of the project to retrieve a badge for", schema = @Schema(type = "string", format = "uuid"), required = true)
             @PathParam("uuid") @ValidUuid String uuid) {
         try (QueryManager qm = new QueryManager()) {
+            if (!isUnauthenticatedBadgeAccessEnabled(qm) && !passesAuthentication()) {
+                return Response.status(Response.Status.UNAUTHORIZED).build();
+            }
+            if (!isUnauthenticatedBadgeAccessEnabled(qm) && !passesAuthorization(qm)) {
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
             final Project project = qm.getObjectByUuid(Project.class, uuid);
             if (project != null) {
-                if (!qm.hasAccess(super.getPrincipal(), project)) {
+                if (!isUnauthenticatedBadgeAccessEnabled(qm) && !qm.hasAccess(super.getPrincipal(), project)) {
                     return Response.status(Response.Status.FORBIDDEN).entity("Access to the specified project is forbidden").build();
                 }
                 final ProjectMetrics metrics = qm.getMostRecentProjectMetrics(project);
@@ -184,19 +311,25 @@ public class BadgeResource extends AlpineResource {
                     content = @Content(schema = @Schema(type = "string"))
             ),
             @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
             @ApiResponse(responseCode = "404", description = "The project could not be found")
     })
-    @PermissionRequired(Permissions.Constants.VIEW_BADGES)
-    @AllowApiKeyInQueryParameter
+    @AuthenticationNotRequired
     public Response getProjectPolicyViolationsBadge(
             @Parameter(description = "The name of the project to query on", required = true)
             @PathParam("name") String name,
             @Parameter(description = "The version of the project to query on", required = true)
             @PathParam("version") String version) {
         try (QueryManager qm = new QueryManager()) {
+            if (!isUnauthenticatedBadgeAccessEnabled(qm) && !passesAuthentication()) {
+                return Response.status(Response.Status.UNAUTHORIZED).build();
+            }
+            if (!isUnauthenticatedBadgeAccessEnabled(qm) && !passesAuthorization(qm)) {
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
             final Project project = qm.getProject(name, version);
             if (project != null) {
-                if (!qm.hasAccess(super.getPrincipal(), project)) {
+                if (!isUnauthenticatedBadgeAccessEnabled(qm) && !qm.hasAccess(super.getPrincipal(), project)) {
                     return Response.status(Response.Status.FORBIDDEN).entity("Access to the specified project is forbidden").build();
                 }
                 final ProjectMetrics metrics = qm.getMostRecentProjectMetrics(project);

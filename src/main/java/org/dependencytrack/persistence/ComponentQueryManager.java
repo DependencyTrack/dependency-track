19e24f5c8c4ecfcc8488dedf3db6dae02ea09b5f
Seb Dangerfield <1449113+sedan07@users.noreply.github.com>
2025-01-03 11:16:15 +0000

    Fix onlyOutdated ungrouped component filtering
    Fixes: #4510
    
    Signed-off-by: Seb Dangerfield <1449113+sedan07@users.noreply.github.com>

--- src/main/java/org/dependencytrack/persistence/ComponentQueryManager.java
+++ src/main/java/org/dependencytrack/persistence/ComponentQueryManager.java
@@ -166,7 +166,7 @@ final class ComponentQueryManager extends QueryManager implements IQueryManager 
                 " && !("+
                 " SELECT FROM org.dependencytrack.model.RepositoryMetaComponent m " +
                 " WHERE m.name == this.name " +
-                " && m.namespace == this.group " +
+                " && (m.namespace == this.group || (m.namespace == null && this.group == null)) " +
                 " && m.latestVersion != this.version " +
                 " && this.purl.matches('pkg:' + m.repositoryType.toString().toLowerCase() + '/%') " +
                 " ).isEmpty()";

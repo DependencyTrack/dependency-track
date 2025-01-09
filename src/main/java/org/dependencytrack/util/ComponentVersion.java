5afabdc8d17e79d907bf650d47303f3e9b21db92
Andre Wagner <andre.wagner@richard-wolf.com>
2025-01-09 11:00:14 +0100

    Use compareTo for implementing equal
    Signed-off-by: Andre Wagner <andre.wagner@richard-wolf.com>

--- src/main/java/org/dependencytrack/util/ComponentVersion.java
+++ src/main/java/org/dependencytrack/util/ComponentVersion.java
@@ -245,49 +245,7 @@ public class ComponentVersion implements Iterable<String>, Comparable<ComponentV
         if (obj == null || !(obj instanceof ComponentVersion)) {
             return false;
         }
-        if (this == obj) {
-            return true;
-        }
-        final ComponentVersion other = (ComponentVersion) obj;
-        final int minVersionMatchLength = (this.versionParts.size() < other.versionParts.size())
-                ? this.versionParts.size() : other.versionParts.size();
-        final int maxVersionMatchLength = (this.versionParts.size() > other.versionParts.size())
-                ? this.versionParts.size() : other.versionParts.size();
-
-        if (minVersionMatchLength == 1 && maxVersionMatchLength >= 3) {
-            return false;
-        }
-
-        //TODO steal better version of code from compareTo
-        for (int i = 0; i < minVersionMatchLength; i++) {
-            final String thisPart = this.versionParts.get(i);
-            final String otherPart = other.versionParts.get(i);
-            if (!thisPart.equals(otherPart)) {
-                return false;
-            }
-        }
-        if (this.versionParts.size() > minVersionMatchLength) {
-            for (int i = minVersionMatchLength; i < this.versionParts.size(); i++) {
-                if (!"0".equals(this.versionParts.get(i))) {
-                    return false;
-                }
-            }
-        }
-
-        if (other.versionParts.size() > minVersionMatchLength) {
-            for (int i = minVersionMatchLength; i < other.versionParts.size(); i++) {
-                if (!"0".equals(other.versionParts.get(i))) {
-                    return false;
-                }
-            }
-        }
-
-        /*
-         *  if (this.versionParts != other.versionParts && (this.versionParts == null || !this.versionParts.equals(other.versionParts))) {
-         *      return false;
-         *  }
-         */
-        return true;
+        return (compareTo((ComponentVersion) obj) == 0);
     }
 
     /**

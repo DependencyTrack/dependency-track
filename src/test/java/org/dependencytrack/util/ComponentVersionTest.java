0bacfe6a172f29d82b7a206ed03ab0009e8ee15d
Andre Wagner <andre.wagner@richard-wolf.com>
2025-01-12 19:23:42 +0100

    Add randomized unittest
    Signed-off-by: Andre Wagner <andre.wagner@richard-wolf.com>

--- src/test/java/org/dependencytrack/util/ComponentVersionTest.java
+++ src/test/java/org/dependencytrack/util/ComponentVersionTest.java
@@ -2,8 +2,12 @@ package org.dependencytrack.util;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.util.Random;
 
 public class ComponentVersionTest {
     @Test
@@ -13,5 +17,47 @@ public class ComponentVersionTest {
 
         Assert.assertEquals(-1, version1.compareTo(version2));
     }
+    /*
+    @Test
+    public void testComplex()  {
+        String filePath = "src/test/resources/version/ubuntu2.txt";
+        List<String> lines = new ArrayList();
+
+        try {
+            lines = Files.readAllLines(Path.of(filePath));
+        }
+        catch (IOException e) {
+            // Handle the exception
+            System.err.println("An error occurred while reading the file:");
+            e.printStackTrace();
+        }
+
+        Random random = new Random();
+
+        while(true) {
+        Integer firstPick = random.nextInt(lines.size());
+        Integer secondPick = random.nextInt(lines.size());
+
+        // Determine lesser and greater
+        String lesser;
+        String greater;
+
+        if (firstPick.compareTo(secondPick) <= 0) {
+            lesser = lines.get(firstPick);
+            greater = lines.get(secondPick);
+        } else {
+            lesser = lines.get(secondPick);
+            greater = lines.get(firstPick);
+        }
+
+        ComponentVersion version_lesser = new ComponentVersion(lesser);
+        ComponentVersion version_greater = new ComponentVersion(greater);
+
+        System.out.println(lesser +  "<<" + greater);
+        if(!(version_lesser.compareTo(version_greater) <= 0))
+            System.out.println("<<<<<<<BUG!!!!!!!!!!!!!!!!!!!!!!");
+        }
+    }
+    */
 }
 

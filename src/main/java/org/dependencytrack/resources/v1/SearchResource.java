8cd0d1e5166a7f10b3ba4f6b3af2d54ac5fbaeea
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2025-05-01 09:25:34 +0200

    make SearchManager a utility class
    Signed-off-by: Steffen Ohrendorf <steffen.ohrendorf@gmx.de>

--- src/main/java/org/dependencytrack/resources/v1/SearchResource.java
+++ src/main/java/org/dependencytrack/resources/v1/SearchResource.java
@@ -28,13 +28,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.apache.commons.lang3.StringUtils;
-import org.dependencytrack.auth.Permissions;
-import org.dependencytrack.resources.v1.vo.BomUploadResponse;
-import org.dependencytrack.search.FuzzyVulnerableSoftwareSearchManager;
-import org.dependencytrack.search.SearchManager;
-import org.dependencytrack.search.SearchResult;
-
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -42,6 +35,13 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.apache.commons.lang3.StringUtils;
+import org.dependencytrack.auth.Permissions;
+import org.dependencytrack.resources.v1.vo.BomUploadResponse;
+import org.dependencytrack.search.FuzzyVulnerableSoftwareSearchManager;
+import org.dependencytrack.search.SearchManager;
+import org.dependencytrack.search.SearchResult;
+
 import java.util.Collections;
 import java.util.Set;
 

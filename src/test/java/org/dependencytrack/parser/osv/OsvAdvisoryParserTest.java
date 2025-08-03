tecan/dependency-track-custom

Fix Issue#5105: OSV ubuntu advisory contains severity without type
The missing type caused a NullPointerException when the OsvAdvisoryParser.parse()
method tried to access the type without checking for null. My fix will check if
type is null and continue the loop without further processing the item.
I have also added a test for this case and fixed testGetEcosystems() in the
OsvDownloadTaskTest that incorrectly checked for null instead of an empty list
after attempting to fetch the ecosystem list.

Signed-off-by: jonbally <19593213+jonbally@users.noreply.github.com>

src/test/java/org/dependencytrack/parser/osv/OsvAdvisoryParserTest.java
@@ -158,4 +158,13 @@ class OsvAdvisoryParserTest {
         Assertions.assertNotNull(advisory);
     }
 
+    @Test
+        // https://github.com/DependencyTrack/dependency-track/issues/5105
+    void testIssue5105() throws Exception {
+        String jsonFile = "src/test/resources/unit/osv.jsons/osv-UBUNTU-CVE-2025-6297.json";
+        String jsonString = new String(Files.readAllBytes(Paths.get(jsonFile)));
+        JSONObject jsonObject = new JSONObject(jsonString);
+        OsvAdvisory advisory = parser.parse(jsonObject);
+        Assertions.assertNotNull(advisory);
+    }
 }

src/test/java/org/dependencytrack/tasks/OsvDownloadTaskTest.java
@@ -455,7 +455,7 @@ class OsvDownloadTaskTest extends PersistenceCapableTest {
     void testGetEcosystems() {
         final var task = new OsvDownloadTask();
         List<String> ecosystems = task.getEcosystems();
-        Assertions.assertNotNull(ecosystems);
+        Assertions.assertFalse(ecosystems.isEmpty());
         Assertions.assertTrue(ecosystems.contains("Maven"));
     }
 

DependencyTrack/dependency-track

Signed-off-by: nscuro <nscuro@protonmail.com>

src/test/java/org/dependencytrack/parser/osv/OsvAdvisoryParserTest.java
@@ -1,9 +1,9 @@
 package org.dependencytrack.parser.osv;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.dependencytrack.parser.osv.model.OsvAdvisory;
 import org.dependencytrack.parser.osv.model.OsvAffectedPackage;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -157,4 +157,13 @@ public class OsvAdvisoryParserTest {
         Assert.assertNotNull(advisory);
     }
 
+    @Test
+        // https://github.com/DependencyTrack/dependency-track/issues/5105
+    public void testIssue5105() throws Exception {
+        String jsonFile = "src/test/resources/unit/osv.jsons/osv-UBUNTU-CVE-2025-6297.json";
+        String jsonString = new String(Files.readAllBytes(Paths.get(jsonFile)));
+        JSONObject jsonObject = new JSONObject(jsonString);
+        OsvAdvisory advisory = parser.parse(jsonObject);
+        Assert.assertNotNull(advisory);
+    }
 }

src/test/java/org/dependencytrack/tasks/OsvDownloadTaskTest.java
@@ -455,7 +455,7 @@ public class OsvDownloadTaskTest extends PersistenceCapableTest {
     public void testGetEcosystems() {
         final var task = new OsvDownloadTask();
         List<String> ecosystems = task.getEcosystems();
-        Assert.assertNotNull(ecosystems);
+        Assert.assertFalse(ecosystems.isEmpty());
         Assert.assertTrue(ecosystems.contains("Maven"));
     }
 
ae4ecf2348687d4a9abccb8e2e0dbc0350d5de3e
Damian Sniezek <snieguu@gmail.com>
2025-08-29 15:52:25 +0200

    improve detection if version is commit sha or release tag for github purl
    Signed-off-by: Damian Sniezek <snieguu@gmail.com>

--- src/test/java/org/dependencytrack/tasks/repositories/GitHubMetaAnalyzerTest.java
+++ src/test/java/org/dependencytrack/tasks/repositories/GitHubMetaAnalyzerTest.java
@@ -19,101 +19,55 @@
 package org.dependencytrack.tasks.repositories;
 
 import com.github.packageurl.PackageURL;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.dependencytrack.model.Component;
 import org.dependencytrack.model.RepositoryType;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import java.util.regex.Pattern;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
 
+@RunWith(JUnitParamsRunner.class)
 public class GitHubMetaAnalyzerTest {
 
-    private static final String VERSION_TYPE_PATTERN = "[a-f,0-9]{6,40}";
-    @Test
-    public void testAnalyzerRelease() throws Exception {
-        final var component = new Component();
-        component.setPurl(new PackageURL("pkg:github/CycloneDX/cdxgen@v9.8.9"));
-
-        final var analyzer = new GithubMetaAnalyzer();
-        Assert.assertTrue(analyzer.isApplicable(component));
-        Assert.assertEquals(RepositoryType.GITHUB, analyzer.supportedRepositoryType());
-
-        MetaModel metaModel = analyzer.analyze(component);
-        Assert.assertNotNull(metaModel.getLatestVersion());
-        Assert.assertTrue(metaModel.getLatestVersion().startsWith("v"));
-        Assert.assertNotNull(metaModel.getPublishedTimestamp());
-    }
-    @Test
-    public void testAnalyzerLongCommit() throws Exception{
-        final var component = new Component();
-        component.setPurl(new PackageURL("pkg:github/CycloneDX/cdxgen@4359dee1b7bd29ee25bc78e358a1254a0277ee96"));
-        Pattern version_pattern = Pattern.compile(VERSION_TYPE_PATTERN);
-
-        final var analyzer = new GithubMetaAnalyzer();
-        Assert.assertTrue(analyzer.isApplicable(component));
-        Assert.assertEquals(RepositoryType.GITHUB, analyzer.supportedRepositoryType());
-
-        MetaModel metaModel = analyzer.analyze(component);
-        Assert.assertNotNull(metaModel.getLatestVersion());
-        Assert.assertTrue(version_pattern.matcher(metaModel.getLatestVersion()).find());
-        Assert.assertNotNull(metaModel.getPublishedTimestamp());
-    }
+    private static final String COMMIT_LATEST_VERSION_TYPE_PATTERN = "^[a-f0-9]{7,40}$";
+    private static final String RELEASE_LATEST_VERSION_TYPE_PATTERN = "^v.*$";
 
     @Test
-    public void testAnalyzerShortCommit() throws Exception{
+    @Parameters(method = "testAnalyzerData")
+    public void testAnalyzerInvalidTag(String purl, String latestVersionPattern, Boolean versionExists) throws Exception {
         final var component = new Component();
-        component.setPurl(new PackageURL("pkg:github/CycloneDX/cdxgen@4359dee"));
-        Pattern version_pattern = Pattern.compile(VERSION_TYPE_PATTERN);
+        component.setPurl(new PackageURL(purl));
 
         final var analyzer = new GithubMetaAnalyzer();
         Assert.assertTrue(analyzer.isApplicable(component));
         Assert.assertEquals(RepositoryType.GITHUB, analyzer.supportedRepositoryType());
 
         MetaModel metaModel = analyzer.analyze(component);
+
         Assert.assertNotNull(metaModel.getLatestVersion());
-        Assert.assertTrue(version_pattern.matcher(metaModel.getLatestVersion()).find());
-        Assert.assertNotNull(metaModel.getPublishedTimestamp());
+        Assert.assertTrue(metaModel.getLatestVersion().matches(latestVersionPattern));
+        if (versionExists) {
+            Assert.assertNotNull(metaModel.getPublishedTimestamp());
+        } else {
+            Assert.assertNull(metaModel.getPublishedTimestamp());
+        }
     }
 
-    @Test
-    public void testAnalyzerNoVersion() throws Exception{
-        final var component = new Component();
-        component.setPurl(new PackageURL("pkg:github/CycloneDX/cdxgen"));
 
-        final var analyzer = new GithubMetaAnalyzer();
-        Assert.assertTrue(analyzer.isApplicable(component));
-        Assert.assertEquals(RepositoryType.GITHUB, analyzer.supportedRepositoryType());
-
-        MetaModel metaModel = analyzer.analyze(component);
-        Assert.assertNotNull(metaModel.getLatestVersion());
-        Assert.assertTrue(metaModel.getLatestVersion().startsWith("v"));
-        Assert.assertNull(metaModel.getPublishedTimestamp());
-    }
-    @Test
-    public void testAnalyzerInvalidCommit() throws Exception{
-        final var component = new Component();
-        component.setPurl(new PackageURL("pkg:github/CycloneDX/cdxgen@0000000"));
-
-        final var analyzer = new GithubMetaAnalyzer();
-        Assert.assertTrue(analyzer.isApplicable(component));
-        Assert.assertEquals(RepositoryType.GITHUB, analyzer.supportedRepositoryType());
-
-        MetaModel metaModel = analyzer.analyze(component);
-        Assert.assertNotNull(metaModel.getLatestVersion());
-        Assert.assertNull(metaModel.getPublishedTimestamp());
-    }
-
-        @Test
-    public void testAnalyzerInvalidRelease() throws Exception{
-        final var component = new Component();
-        component.setPurl(new PackageURL("pkg:github/CycloneDX/cdxgen@invalid-release"));
-
-        final var analyzer = new GithubMetaAnalyzer();
-        Assert.assertTrue(analyzer.isApplicable(component));
-        Assert.assertEquals(RepositoryType.GITHUB, analyzer.supportedRepositoryType());
-
-        MetaModel metaModel = analyzer.analyze(component);
-        Assert.assertNotNull(metaModel.getLatestVersion());
-        Assert.assertNull(metaModel.getPublishedTimestamp());
+    static Object[] testAnalyzerData() {
+        return new Object[]{
+                new Object[]{"pkg:github/CycloneDX/cdxgen@v9.8.9", RELEASE_LATEST_VERSION_TYPE_PATTERN, TRUE},
+                new Object[]{"pkg:github/CycloneDX/cdxgen@4359dee1b7bd29ee25bc78e358a1254a0277ee96", COMMIT_LATEST_VERSION_TYPE_PATTERN, TRUE},
+                new Object[]{"pkg:github/CycloneDX/cdxgen@4359dee", COMMIT_LATEST_VERSION_TYPE_PATTERN, TRUE},
+                new Object[]{"pkg:github/CycloneDX/cdxgen", RELEASE_LATEST_VERSION_TYPE_PATTERN, FALSE},
+                new Object[]{"pkg:github/CycloneDX/cdxgen@0000000", COMMIT_LATEST_VERSION_TYPE_PATTERN, FALSE},
+                new Object[]{"pkg:github/CycloneDX/cdxgen@invalid-release", RELEASE_LATEST_VERSION_TYPE_PATTERN, FALSE},
+                new Object[]{"pkg:github/google/gtm-session-fetcher@3.1.0", RELEASE_LATEST_VERSION_TYPE_PATTERN, FALSE},
+                new Object[]{"pkg:github/boostorg/boost@1.88.0.beta1", "boost-.*", FALSE}
+        };
     }
 }

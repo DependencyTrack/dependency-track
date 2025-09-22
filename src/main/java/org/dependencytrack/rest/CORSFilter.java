9f86cddab39ec9bf087c65beff037fab5baddcf2
Youssef El <105607242+JoesphK@users.noreply.github.com>
2025-09-22 11:48:47 +0300

    Update CORSFilter.java
    Updated the cors filter again
    
    Signed-off-by: Youssef El <105607242+JoesphK@users.noreply.github.com>

--- src/main/java/org/dependencytrack/rest/CORSFilter.java
+++ src/main/java/org/dependencytrack/rest/CORSFilter.java
@@ -5,13 +5,16 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) OWASP Foundation. All Rights Reserved.
  */
 
 package org.dependencytrack.rest;
@@ -33,3 +36,4 @@ public class CORSFilter implements ContainerResponseFilter {
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     }
 }
+

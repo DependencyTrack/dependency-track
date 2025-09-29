870f67f8d6ebfd395e921c9fa2f76f04dcd56812
nscuro <nscuro@protonmail.com>
2025-09-29 21:59:46 +0200

    Bump SPDX license list to 3.27.0
    Signed-off-by: nscuro <nscuro@protonmail.com>

--- src/test/java/org/dependencytrack/persistence/DefaultObjectGeneratorTest.java
+++ src/test/java/org/dependencytrack/persistence/DefaultObjectGeneratorTest.java
@@ -48,7 +48,7 @@ public class DefaultObjectGeneratorTest extends PersistenceCapableTest {
         Method method = generator.getClass().getDeclaredMethod("loadDefaultLicenses");
         method.setAccessible(true);
         method.invoke(generator);
-        Assert.assertEquals(757, qm.getAllLicensesConcise().size());
+        Assert.assertEquals(778, qm.getAllLicensesConcise().size());
     }
 
     @Test

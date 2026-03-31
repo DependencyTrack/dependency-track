381c8ecdcf7b16773ad7c8bbc743833eac8b3372
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2026-03-31 08:06:27 +0200

    avoid NPE when computing pkgType
    Signed-off-by: Steffen Ohrendorf <steffen.ohrendorf@gmx.de>

--- src/main/java/org/dependencytrack/tasks/scanners/TrivyAnalysisTask.java
+++ src/main/java/org/dependencytrack/tasks/scanners/TrivyAnalysisTask.java
@@ -245,15 +245,16 @@ public class TrivyAnalysisTask extends BaseComponentAnalyzerTask implements Subs
                         String arch = null;
                         Integer epoch = null;
 
-                        if (component.getPurl().getQualifiers() != null) {
-                            arch = component.getPurl().getQualifiers().get("arch");
+                        final var qualifiers = component.getPurl().getQualifiers();
+                        if (qualifiers != null) {
+                            arch = qualifiers.get("arch");
 
-                            String tmpEpoch = component.getPurl().getQualifiers().get("epoch");
+                            String tmpEpoch = qualifiers.get("epoch");
                             if (tmpEpoch != null) {
                                 epoch = Integer.parseInt(tmpEpoch);
                             }
 
-                            String distro = component.getPurl().getQualifiers().get("distro");
+                            String distro = qualifiers.get("distro");
 
                             if (distro != null) {
                                 pkgType = URLDecoder.decode(distro, StandardCharsets.UTF_8);
@@ -272,7 +273,7 @@ public class TrivyAnalysisTask extends BaseComponentAnalyzerTask implements Subs
                             } else if (!pkgType.contains("-") && property.getPropertyName().equals("trivy:PkgType")) {
                                 pkgType = property.getPropertyValue();
 
-                                String distro = component.getPurl().getQualifiers().get("distro");
+                                String distro = qualifiers == null ? null : qualifiers.get("distro");
 
                                 if (distro != null) {
                                     pkgType += "-" + URLDecoder.decode(distro, StandardCharsets.UTF_8);

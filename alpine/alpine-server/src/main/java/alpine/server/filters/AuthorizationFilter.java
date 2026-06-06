DependencyTrack/dependency-track

commit c8d55d20a913002d5aa50e152d4807df49cc5fa5

Populate effective permissions for all authenticated requests
Allows resources that do not *require* a permission to be accessed to still check the principal's effective permissions without having to query the DB. For example `/v1/team/visible` is accessible by all authenticated principals, but its response content still depends on which permissions those principals hold.

Replaces the separate `AuthenticationFeature` and `AuthorizationFeature` with a single `AuthFeature`. Since the condition for both is now identical, there is no need for separate features anymore.

This change surfaced a few resources where `AlpineRequest` was deliberately not passed to `new QueryManager` (which is necessary for ACL-aware query filtering), in order to perform "lookup project and assert access" patterns. To fix this, we introduce a mechanism to explicitly bypass portfolio ACL when needed, using `ProjectAccess.unrestricted(() -> ...)`.

Signed-off-by: nscuro <nscuro@protonmail.com>

alpine/alpine-server/src/main/java/alpine/server/filters/AuthorizationFilter.java
@@ -43,7 +43,7 @@ import java.util.Set;
  * through this filter have the necessary permissions to do so.
  *
  * @author Steve Springett
- * @see AuthorizationFeature
+ * @see AuthFeature
  * @since 1.0.0
  */
 @Priority(Priorities.AUTHORIZATION)
@@ -68,28 +68,27 @@ public class AuthorizationFilter implements ContainerRequestFilter {
         try (final var qm = new AlpineQueryManager()) {
             effectivePermissions = qm.getEffectivePermissions(principal);
         }
+        requestContext.setProperty(EFFECTIVE_PERMISSIONS_PROPERTY, effectivePermissions);
 
         final PermissionRequired annotation = resourceInfo.getResourceMethod().getDeclaredAnnotation(PermissionRequired.class);
-        final Set<String> permissions = Set.of(annotation.value());
-
-        final boolean hasNoRequiredPermission = Collections.disjoint(permissions, effectivePermissions);
-        if (hasNoRequiredPermission) {
-            final String requestUri = requestContext.getUriInfo().getRequestUri().toString();
-            final String requestPrincipal;
-
-            switch (principal) {
-                case ApiKey apiKey -> requestPrincipal = "API Key " + apiKey.getMaskedKey();
-                case User user -> requestPrincipal = user.getUsername();
-                default -> throw new IllegalStateException("Unexpected principal type: " + principal.getClass().getName());
-            }
-
-            LOGGER.info(SecurityMarkers.SECURITY_FAILURE, "Unauthorized access attempt made by %s to %s"
-                    .formatted(requestPrincipal, requestUri));
-
-            throw new ForbiddenException(Response.status(Response.Status.FORBIDDEN).build());
-        } else {
-            requestContext.setProperty(EFFECTIVE_PERMISSIONS_PROPERTY, effectivePermissions);
+        if (annotation == null) {
+            return;
         }
+
+        if (!Collections.disjoint(Set.of(annotation.value()), effectivePermissions)) {
+            return;
+        }
+
+        final String requestPrincipal = switch (principal) {
+            case ApiKey apiKey -> "API Key " + apiKey.getMaskedKey();
+            case User user -> user.getUsername();
+            default -> throw new IllegalStateException("Unexpected principal type: " + principal.getClass().getName());
+        };
+
+        LOGGER.info(SecurityMarkers.SECURITY_FAILURE, "Unauthorized access attempt made by %s to %s"
+                .formatted(requestPrincipal, requestContext.getUriInfo().getRequestUri().toString()));
+
+        throw new ForbiddenException(Response.status(Response.Status.FORBIDDEN).build());
     }
 
 }

alpine/alpine-server/src/test/java/alpine/server/filters/AuthorizationFilterTest.java
@@ -60,6 +60,15 @@ public class AuthorizationFilterTest extends JerseyTest {
                     getAlpineRequest().getEffectivePermissions())).build();
         }
 
+        @GET
+        @Path("/unprotected")
+        @Produces(MediaType.APPLICATION_JSON)
+        public Response getUnprotected() {
+            return Response.ok(Map.of(
+                    "effectivePermissions",
+                    getAlpineRequest().getEffectivePermissions())).build();
+        }
+
     }
 
     @AfterEach
@@ -72,9 +81,34 @@ public class AuthorizationFilterTest extends JerseyTest {
     protected Application configure() {
         forceSet(TestProperties.CONTAINER_PORT, "0");
         return new ResourceConfig(TestResource.class)
-                .register(AuthenticationFeature.class)
-                .register(AuthorizationFeature.class)
+                .register(AuthFeature.class)
                 .register(ApiFilter.class);
+    }
+
+    @Test
+    void shouldPopulateEffectivePermissionsOnRequestsWithoutPermissionRequired() {
+        final String apiKey;
+        try (final var qm = new AlpineQueryManager()) {
+            final Permission bazPermission = qm.createPermission("BAZ", null);
+
+            final Team team = qm.createTeam("foo");
+            team.getPermissions().add(bazPermission);
+
+            apiKey = qm.createApiKey(team).getKey();
+        }
+
+        final Response response = target("/unprotected")
+                .request()
+                .header("X-Api-Key", apiKey)
+                .get();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThatJson(response.readEntity(String.class))
+                .withOptions(Option.IGNORING_ARRAY_ORDER)
+                .isEqualTo(/* language=JSON */ """
+                        {
+                          "effectivePermissions": ["BAZ"]
+                        }
+                        """);
     }
 
     @Test

commit 76fa9d7239a2637f8c863ff88aa29a8ea44b3394

Fix AuthZ being enforced on CORS preflight requests
Fixes a regression introduced in c8d55d20a913002d5aa50e152d4807df49cc5fa5.

Signed-off-by: nscuro <nscuro@protonmail.com>

alpine/alpine-server/src/main/java/alpine/server/filters/AuthorizationFilter.java
@@ -24,6 +24,7 @@ import alpine.persistence.AlpineQueryManager;
 import alpine.server.auth.PermissionRequired;
 import jakarta.annotation.Priority;
 import jakarta.ws.rs.ForbiddenException;
+import jakarta.ws.rs.HttpMethod;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
@@ -58,6 +59,12 @@ public class AuthorizationFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) {
+        // Bypass authorization for CORS preflight.
+        // AuthenticationFilter does the same, so no principal is available to authorize against.
+        if (HttpMethod.OPTIONS.equals(requestContext.getMethod())) {
+            return;
+        }
+
         final Principal principal = (Principal) requestContext.getProperty("Principal");
         if (principal == null) {
             LOGGER.info(SecurityMarkers.SECURITY_FAILURE, "A request was made without the assertion of a valid user principal");

alpine/alpine-server/src/test/java/alpine/server/filters/AuthorizationFilterTest.java
@@ -86,6 +86,14 @@ public class AuthorizationFilterTest extends JerseyTest {
     }
 
     @Test
+    void shouldAllowCorsPreflightWithoutAuthentication() {
+        final Response response = target("/unprotected")
+                .request()
+                .options();
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+
+    @Test
     void shouldPopulateEffectivePermissionsOnRequestsWithoutPermissionRequired() {
         final String apiKey;
         try (final var qm = new AlpineQueryManager()) {